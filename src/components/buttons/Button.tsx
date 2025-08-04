e2b907eb961950edca49af4638299715092dda52
da10nagi <meiji.dai1math@gmail.com>
2025-08-05 06:03:15 +0900

    ボタンアニメーション、レスポンシブ対応

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from "next/link";
 
+
 type ButtonProps = {
     className?: string;
     children: React.ReactNode;
@@ -11,8 +12,10 @@ export default function Button({ className, children, href }: ButtonProps) {
     return(
         <div className='relative'>
            <Link href={href}>
-                <div className="-rotate-6 rounded-full border-4 border-secondary py-4 absolute py-2 px-24" />
-                <div className={`font-bold rounded-full hover:bg-gray-300 transition duration-300 bg-accent text-black py-2 px-16 absolute ${className}`}>
+                <div className={`-rotate-6 rounded-full border-2 sm:border-4  border-secondary  absolute py-3 px-20 sm:px-30 text-center ${className}`}>
+                    <p className='opacity-0'>{children}</p>
+                </div>
+                <div className={`font-bold rounded-full hover:bg-accent-700 transition duration-300 bg-accent text-black py-3 px-20 sm:px-30 absolute ${className}`}>
                     {children}                        
                 </div>
             </Link>
