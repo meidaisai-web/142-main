827608c95fcb826cc7449be9e306f279b9e10392
da10nagi <meiji.dai1math@gmail.com>
2025-08-05 00:55:30 +0900

    ボタンを追加

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Link from "next/link";
+
+type ButtonProps = {
+    className?: string;
+    name: React.ReactNode;
+    href: string;
+}
+
+export default function Button({ className, name, href }: ButtonProps) {
+    return(
+        <div className='relative'>
+           <Link href={href}>
+                <div className="-rotate-6 rounded-full border-4 border-secondary py-4 absolute py-2 px-24" />
+                <div className={`font-bold rounded-full hover:bg-gray-300 transition duration-300 bg-accent text-black py-2 px-16 absolute ${className}`}>
+                    {name}                        
+                </div>
+            </Link>
+        </div>     
+    )
+}
