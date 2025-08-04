ad56aff6692f875b9937092942bf9efb4e4a5995
da10nagi <meiji.dai1math@gmail.com>
2025-08-05 01:06:04 +0900

    nameをchildrenに変更

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,17 +3,17 @@ import Link from "next/link";
 
 type ButtonProps = {
     className?: string;
-    name: React.ReactNode;
+    children: React.ReactNode;
     href: string;
 }
 
-export default function Button({ className, name, href }: ButtonProps) {
+export default function Button({ className, children, href }: ButtonProps) {
     return(
         <div className='relative'>
            <Link href={href}>
                 <div className="-rotate-6 rounded-full border-4 border-secondary py-4 absolute py-2 px-24" />
                 <div className={`font-bold rounded-full hover:bg-gray-300 transition duration-300 bg-accent text-black py-2 px-16 absolute ${className}`}>
-                    {name}                        
+                    {children}                        
                 </div>
             </Link>
         </div>     
