e90cfd33ced11ccd5d62765d36fdfdff37ce231a
Rikuto Hagiya <rikuto.hagiya@gmail.com>
2025-08-16 16:42:44 +0900

    全体的に変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import PageTitle from "@/components/texts/PageTitle";
-
 export default function Home() {
   return (
     <div>
--- src/components/texts/PageTitle.tsx
+++ src/components/texts/PageTitle.tsx
@@ -8,27 +8,30 @@ type PageTitleProps = {
 
 export default function PageTitle({ children, className = "" }: PageTitleProps) {
   return (
-    <div className={`relative w-full p-3 md:p-6 text-center ${className}`}>
+    <div className={`relative w-full p-6 md:p-12 text-center ${className}`}>
       {/* タイトル本体 */}
-      <h1 className={`inline-block text-white text-xl sm:text-2xl md:text-4xl lg:text-6xl font-extrabold whitespace-nowrap relative p-3 md:p-6 ${delaGothic.className}`}>
-        {children}
-        {/* 下部の黄ライン */}
-        <span
-            aria-hidden
-            className="absolute left-1/2 -translate-x-1/2 -bottom-0 md:-bottom-3 lg:-bottom-5 block h-0.5 md:h-1 bg-accent"
-            style={{ width: "50%" }}
-        />
+      <h1 className={`relative inline-flex items-center justify-center gap-4 md:gap-8 text-white text-xl sm:text-2xl md:text-4xl font-extrabold ${delaGothic.className}`}>
         {/* 左側の斜線 */}
         <span
-            aria-hidden
-            className="absolute left-0 bottom-4 md:bottom-4 lg:bottom-5 -translate-x-5/6 block h-0.5 md:h-1.5 bg-secondary rotate-[30deg]"
-            style={{ width: "40%" }}
+          aria-hidden
+          className="relative block w-16 md:w-32 h-0.5 md:h-1.5 bg-secondary rotate-[30deg] shrink-0"
         />
+        
+        {/* 中央のテキスト部分 */}
+        <span className="relative whitespace-pre-wrap text-center dela-font">
+          {children}
+          {/* 下部の黄ライン */}
+          <span
+            aria-hidden
+            className="absolute left-1/2 -translate-x-1/2 -bottom-2 md:-bottom-4 block h-0.5 md:h-1 bg-accent"
+            style={{ width: "50%" }}
+          />
+        </span>
+        
         {/* 右側の斜線 */}
         <span
-            aria-hidden
-            className="absolute right-0 bottom-4 md:bottom-4 lg:bottom-5 translate-x-5/6 block h-0.5 md:h-1.5 bg-secondary rotate-[-30deg]"
-            style={{ width: "40%" }}
+          aria-hidden
+          className="relative block w-16 md:w-32 h-0.5 md:h-1.5 bg-secondary rotate-[-30deg] shrink-0"
         />
       </h1>
     </div>
