ee9534c7de68e7d2bbd08c906b28a3a46c127794
Rikuto Hagiya <rikuto.hagiya@gmail.com>
2025-08-28 01:48:13 +0900

    青斜線削除

--- src/components/texts/PageTitle.tsx
+++ src/components/texts/PageTitle.tsx
@@ -11,12 +11,6 @@ export default function PageTitle({ children, className = "" }: PageTitleProps) 
     <div className={`relative w-full p-6 md:p-12 text-center ${className}`}>
       {/* タイトル本体 */}
       <h1 className={`relative inline-flex items-center justify-center gap-4 md:gap-8 text-white text-xl sm:text-2xl md:text-4xl font-extrabold ${delaGothic.className}`}>
-        {/* 左側の斜線 */}
-        <span
-          aria-hidden
-          className="relative block w-16 md:w-32 h-0.5 md:h-1.5 bg-secondary rotate-[30deg] shrink-0"
-        />
-        
         {/* 中央のテキスト部分 */}
         <span className="relative whitespace-pre-wrap text-center dela-font">
           {children}
@@ -27,12 +21,6 @@ export default function PageTitle({ children, className = "" }: PageTitleProps) 
             style={{ width: "50%" }}
           />
         </span>
-        
-        {/* 右側の斜線 */}
-        <span
-          aria-hidden
-          className="relative block w-16 md:w-32 h-0.5 md:h-1.5 bg-secondary rotate-[-30deg] shrink-0"
-        />
       </h1>
     </div>
   );
