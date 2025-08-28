a6cc935b881f6d4db7f47c23333198cbc8990ad7
Rikuto Hagiya <rikuto.hagiya@gmail.com>
2025-08-28 17:41:22 +0900

    PageTitle

--- src/components/texts/PageTitle.tsx
+++ src/components/texts/PageTitle.tsx
@@ -8,7 +8,7 @@ type PageTitleProps = {
 
 export default function PageTitle({ children, className = "" }: PageTitleProps) {
   return (
-    <div className={`relative w-full p-6 md:p-12 text-center ${className}`}>
+    <div className={`relative w-full p-9 md:p-15 text-center ${className}`}>
       {/* タイトル本体 */}
       <h1 className={`relative inline-flex items-center justify-center gap-4 md:gap-8 text-white text-xl sm:text-2xl md:text-4xl font-extrabold ${delaGothic.className}`}>
         {/* 中央のテキスト部分 */}
@@ -17,8 +17,7 @@ export default function PageTitle({ children, className = "" }: PageTitleProps) 
           {/* 下部の黄ライン */}
           <span
             aria-hidden
-            className="absolute left-1/2 -translate-x-1/2 -bottom-2 md:-bottom-4 block h-0.5 md:h-1 bg-accent"
-            style={{ width: "50%" }}
+            className="absolute left-1/2 -translate-x-1/2 -bottom-2 md:-bottom-4 block h-1 bg-accent rounded-[2px] md:rounded-[2px] w-1/3"
           />
         </span>
       </h1>
