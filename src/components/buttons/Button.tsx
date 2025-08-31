9145ee96cde7e3a538d07d19fbbf913199e40541
tiger121005 <itiger4649@gmail.com>
2025-08-31 22:42:55 +0900

    404ページを作成

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -12,10 +12,10 @@ export default function Button({ className, children, href }: ButtonProps) {
     return (
         <div className={`${className} relative p-5 h-24`}>
             <Link href={href} className="absolute -translate-x-1/2 left-1/2 whitespace-nowrap">
-                <div className='-rotate-6 rounded-full border-4 border-secondary py-3 px-20 sm:px-30 text-center absolute -translate-x-1/2 left-1/2'>
+                <div className='-rotate-3 rounded-full border-4 border-secondary py-3 px-20 sm:px-30 text-center absolute -translate-x-1/2 left-1/2'>
                     <p className='opacity-0'>{children}</p>
                 </div>
-                <div className='font-bold rounded-full hover:bg-accent-700 transition duration-300 bg-accent text-black py-3 px-20 sm:px-30 absolute z-10 -translate-x-1/2 left-1/2'>
+                <div className='font-bold rounded-full hover:bg-accent-700 transition duration-100 bg-accent text-black py-3 px-20 sm:px-30 absolute z-10 -translate-x-1/2 left-1/2'>
                     {children}
                 </div>
             </Link>
