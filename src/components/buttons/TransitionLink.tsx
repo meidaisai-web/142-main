4f29c8b0f2de49a8a35dd301946d62b9b905c2b4
tiger121005 <itiger4649@gmail.com>
2025-08-31 22:27:16 +0900

    リンクボタンのコンポーネントを作成

---
+++ src/components/buttons/TransitionLink.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+type TransitionLinkProps = {
+  href: string;
+  children: React.ReactNode;
+  // true、もしくはtargetBlankを指定した場合は新しいタブで開く
+  targetBlank?: boolean;
+  className?: string;
+};
+
+export default function TransitionLink({
+  href,
+  children,
+  className,
+  targetBlank,
+}: TransitionLinkProps) {
+  return (
+    <Link
+      href={href}
+      className={`text-accent border-b border-accent hover:text-secondary hover:border-secondary ${className}`}
+      target={targetBlank ? "_blank" : "_self"}
+    >
+      {children}
+    </Link>
+  );
+}
