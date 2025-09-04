f0f46869f8119fde15e75a31f2ecc6c8304a4685
ShioriTajima <chiroru7215@gmail.com>
2025-09-04 18:50:13 +0900

    フォントサイズを調整した

--- src/components/texts/IndexTitle.tsx
+++ src/components/texts/IndexTitle.tsx
@@ -7,7 +7,7 @@ type IndexTitleProps = {
 
 export default function IndexTitle({ children }: IndexTitleProps) {
     return (
-        <h1 className={`w-full text-center text-2xl sm:text-4xl whitespace-nowrap p-4 pt-8 ${delaGothic.className}`}>
+        <h1 className={`w-full text-center text-4xl sm:text-4xl whitespace-nowrap p-4 pt-8 ${delaGothic.className}`}>
             {children}
         </h1>
     );
