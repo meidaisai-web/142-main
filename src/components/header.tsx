7a9990df13ccbfe0ca477996e912226991a3d7c5
SaraHoriguchi <shalaikukou@gmail.com>
2025-09-06 22:44:09 +0900

    ロゴ・余白の修正

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-full bg-primary-trans flex justify-between items-center px-5 fixed">
+    <header className="w-full bg-primary-trans flex justify-between items-center px-5 fixed opacity-100 bg primary">
       <Image 
       src="/images/svg/logo-white.svg" 
       alt="Logo" 
@@ -11,31 +11,31 @@ export default function Header() {
       height={40} 
       />
 
-      <ul className="flex gap-4 items-center">
+      <ul className="flex gap-8 items-center py-5">
         {/* 35rem以上 */}
         <li className="max-[37rem]:hidden">
-          <Link href="/guest" className="flex gap-15 p-8 text-white font-bold">ご来場者のみなさまへ</Link>
+          <Link href= "/guest" className="flex gap-5 my-auto text-white font-bold">ご来場者のみなさまへ</Link>
         </li>
         <li className="max-[37rem]:hidden">
-          <Link href="/search" className="flex gap-15 p-8 text-white font-bold">特集</Link>
+          <Link href="/search" className="flex gap-5 my-auto text-white font-bold">特集</Link>
         </li>
         <li className="max-[37rem]:hidden">
-          <Link href="/participation"  className="flex gap-15 p-8 text-white font-bold">企画を探す</Link>
+          <Link href="/participation"  className="flex gap-5 my-auto text-white font-bold">企画を探す</Link>
         </li>
 
         {/* 35rem以下 */}
         <li className="hidden max-[37rem]:flex gap-2 items-center">
           
           <Image
-            src="/images/svg/caution.png"
-            alt="caution"
+            src="/images/svg/people2.png"
+            alt="people2"
             width={30}
             height={30}
           />
           <Image
-            src="/images/svg/book.png"
-            alt="Book"
-            width={40}
+            src="/images/svg/point2.png"
+            alt="Point2"
+            width={26}
             height={30}
             style={{ margin: "3px 15px 3px 15px" }}
           />
