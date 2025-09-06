c92459ee11770f5e984ab2a1ecc85aa2586798bc
Ena Sakai <enamar2807@gmail.com>
2025-09-06 12:13:05 +0900

    コンフリクトを解消

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,41 +1,96 @@
 import Image from "next/image";
 export default function Footer() {
   return (
-    <footer className="footer w-screen bg-secondary pb-10">
-      <div className="flex flex-col items-center">
-        <div className="flex justify-center mt-10 mx-10">
-          <Image src="/images/svg/logo-title-concept-white.svg" alt="Logo" width={190} height={45} />
+    <footer className="footer w-full bg-secondary pt-15 pb-10">
+      <div className="mx-auto flex flex-col items-center">
+        <div className="flex justify-center mx-10">
+          <Image src="/images/svg/logo-title-concept-white.svg" alt="Logo" width={240} height={55} className="w-45 md:w-60 h-auto" />
         </div>
-        <div className="flex justify-between my-8 w-full px-15 max-w-96">
-          <Image src="/images/svg/Instagram_White.svg" alt="instagram" width={26} height={23}  />
-          <Image src="/images/svg/X-white.svg" alt="x" width={25} height={20} />
-          <Image src="/images/svg/youtube-white.svg" alt="youtube" width={27} height={20}/>
-          <Image src="/images/svg/TikTok.svg" alt="TikTok" width={25} height={20} />
+        <div className="flex my-11 px-15 max-w-96 gap-10 md:gap-15 justify-center">
+          <a
+            href="https://www.instagram.com/meidaisai"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="relative w-8 h-8 md:w-11 md:h-11">
+              <Image
+                src="/images/svg/Instagram_White.svg"
+                alt="instagram"
+                fill
+                className="object-contain"
+              />
+            </div>
+          </a>
+
+          <a
+            href="https://x.com/meidaisai"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="relative w-8 h-8 md:w-11 md:h-11">
+              <Image
+                src="/images/svg/X-white.svg"
+                alt="x"
+                fill
+                className="object-contain"
+              />
+            </div>
+          </a>
+
+          <a
+            href="https://www.youtube.com/@meidaisainetpr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="relative w-8 h-8 md:w-11 md:h-11">
+              <Image
+                src="/images/svg/youtube-white.svg"
+                alt="youtube"
+                fill
+                className="object-contain"
+              />
+            </div>
+          </a>
+
+          <a
+            href="https://www.tiktok.com/@meidaisai_"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="relative w-8 h-8 md:w-11 md:h-11">
+              <Image
+                src="/images/svg/TikTok.svg"
+                alt="TikTok"
+                fill
+                className="object-contain"
+              />
+            </div>
+          </a>
         </div>
 
-        <div className="flex justify-center gap-4 sm:gap-6 lg:gap-8 text-white text-xs px-5">
+        <div className="flex justify-center gap-4 sm:gap-6 lg:gap-8 text-white text-xs px-5 list-none">
           <div>
-            <a className="border-b-[1.5px] border-b-transparent hover:border-b-white duration-250">
+            <li className="border-b-[1.5px] border-b-transparent hover:border-b-white duration-250">
               お問い合わせ
-            </a>
+            </li>
           </div>
           <div>
-            <a className="border-b-[1.5px] border-b-transparent hover:border-b-white duration-250">
+            <li className="border-b-[1.5px] border-b-transparent hover:border-b-white duration-250">
               リンク
-            </a>
+            </li>
           </div>
           <div>
-            <a className="border-b-[1.5px] border-b-transparent hover:border-b-white duration-250">
+            <li className="border-b-[1.5px] border-b-transparent hover:border-b-white duration-250">
               サイトマップ
-            </a>
+            </li>
           </div>
           <div>
-            <a className="border-b-[1.5px] border-b-transparent hover:border-b-white duration-250">
+            <li className="border-b-[1.5px] border-b-transparent hover:border-b-white duration-250">
               プライバシーポリシー
-            </a>
+            </li>
           </div>
         </div>
-        <div className="justify-center flex-col text-white text-sm font-copyright font-bold pt-8">
+        <div className="justify-center flex-col text-white text-sm font-copyright font-bold pt-9">
           <p>©第141回明大祭実行委員会</p>
         </div>
       </div>
