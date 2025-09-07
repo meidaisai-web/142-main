a7a700bc591e5024f9fb8c3fce61cb59ca8153a3
tiger121005 <itiger4649@gmail.com>
2025-09-08 01:43:34 +0900

    たりてないデータを一部追加

--- src/utils/adData.ts
+++ src/utils/adData.ts
@@ -1,14 +1,14 @@
 import { Ad } from "./models/AdType";
 
 export const adData: Ad[] = [
-    { url: "https://meidaisai.jp", src: "/images/ad/adachi.jpg", id: "adachi" },//足立自動車学校
-    { url: "https://meidaisai.jp", src: "/images/ad/towa.jpg", id: "towa" },//東和エンジニアリング
+    { url: "https://www.adachi-driving-school.com/", src: "/images/ad/adachi.jpg", id: "adachi" },//足立自動車学校
+    { url: "https://www.towaeng.co.jp/", src: "/images/ad/towa.jpg", id: "towa" },//東和エンジニアリング
     { url: "https://www.axona-aichi.com/", src: "/images/ad/aichi.jpg", id: "aichi" },//愛知株式会社
     { url: "https://www.ginapartners.com/563252841732/internship-university-student?utm_source=meiji-festival&utm_medium=banner&utm_campaign=internship-abroad", src: "/images/ad/gina.jpg", id: "gina" },//Gina&Partners
-    { url: "https://meidaisai.jp", src: "/images/ad/ichikura.jpg", id: "ichikura" },//株式会社一蔵
+    { url: "https://furisode-ichikura.jp/", src: "/images/ad/ichikura.jpg", id: "ichikura" },//株式会社一蔵
     { url: "https://www.fem-produce.co.jp/", src: "/images/ad/fem.jpg", id: "fem" },//フェム
     { url: "https://www.fbond.net", src: "/images/ad/kanda.jpg", id: "kanda" },//東京神田ゼミ
-    { url: "https://meidaisai.jp", src: "/images/ad/matsubara.png", id: "matsubara" },//松原皮膚科
+    { url: "https://www.matsubarahifuka.com/", src: "/images/ad/matsubara.png", id: "matsubara" },//松原皮膚科
     { url: "https://cpa-net.jp/contact/request.php?utm_source=shanon&utm_medium=email&utm_campaign=2025_meidaisai", src: "/images/ad/cpa.jpg", id: "cpa" },//CPA
 ];
 
@@ -18,5 +18,5 @@ export const smallAdData: Ad[] = [
     { url: "https://meidaisai.jp", src: "/images/ad/riso.jpg", id: "riso" },//株式会社リソー教育
     { url: "https://www.senko.co.jp/jp/", src: "/images/ad/senko.jpg", id: "senko" },//センコー
     { url: "https://www.densetsuko.co.jp/", src: "/images/ad/denko.jpg", id: "denko" },//日本電設工業
-    { url: "https://meidaisai.jp", src: "/images/ad/suruga.jpg", id: "suruga" },//駿河台学園
+    { url: "https://www2.sundai.ac.jp/", src: "/images/ad/suruga.jpg", id: "suruga" },//駿河台学園
 ];
