adbd010e30e92fcadb710b7d059b8d35e0f45647
tiger121005 <itiger4649@gmail.com>
2025-09-11 16:00:08 +0900

    住所だけでも表示できるようにする

--- src/app/(footer)/contact/page.tsx
+++ src/app/(footer)/contact/page.tsx
@@ -11,15 +11,15 @@ export default function Page() {
             <PageContainer>
                 <Text className="!text-center mt-5">明大祭や明大祭実行委員会に関するご質問がございましたら､下記のお問い合わせ先までご連絡ください｡</Text>
                 <SectionTitle>所在地</SectionTitle>
-                <ContactView department="" showPhone showAddress/>
+                <ContactView department="" showPhone showAddress noPadding />
                 <SectionTitle>第141回明大祭への参加に関するお問い合わせ</SectionTitle>
-                <ContactView department="参加団体局" mail="141st-sankadantai@meidaisai.jp" />
+                <ContactView department="参加団体局" mail="141st-sankadantai@meidaisai.jp" noPadding />
                 <SectionTitle>企業・校友・界隈地域のみなさまのお問い合わせ</SectionTitle>
-                <ContactView department="渉外局" mail="shogai@meidaisai.jp" />
+                <ContactView department="渉外局" mail="shogai@meidaisai.jp" noPadding />
                 <SectionTitle>メディアのみなさまのお問い合わせ</SectionTitle>
-                <ContactView department="広報局" mail="141st-kouhou@meidaisai.jp" />
+                <ContactView department="広報局" mail="141st-kouhou@meidaisai.jp" noPadding />
                 <SectionTitle>その他第141回明大祭に関するお問い合わせ</SectionTitle>
-                <ContactView department="" mail="141st4yaku@meidaisai.jp" />
+                <ContactView department="" mail="141st4yaku@meidaisai.jp" noPadding />
             </PageContainer>
         </div>
     );
