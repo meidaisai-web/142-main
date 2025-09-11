99a04d3a8e05c353f443aa051818d05d2f61baa6
tiger121005 <itiger4649@gmail.com>
2025-09-11 16:04:30 +0900

    テキストのスペース調整

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -10,9 +10,9 @@ export default function MediaPage() {
       <PageTitle>メディアのみなさまへ</PageTitle>
       <PageContainer>
         <SectionTitle>ご挨拶</SectionTitle>
-        <Text>この度は第141回明大祭にご関心をお寄せいただき、誠にありがとうございます。第141回明大祭に関して取材をお考えの方は、下記のお問い合わせ先までご連絡ください。</Text>
-        <Text>また、明大祭や明大祭実行委員会の写真や映像などを使用される場合も同様に、下記のお問い合わせ先までご連絡いただきますようお願い申し上げます。</Text>
-        <Text>※明大祭当日の取材につきましては、別途事前申請が必要となりますので、その旨をお伝えください。</Text>
+        <Text moreTopPadding>この度は第141回明大祭にご関心をお寄せいただき、誠にありがとうございます。第141回明大祭に関して取材をお考えの方は、下記のお問い合わせ先までご連絡ください。</Text>
+        <Text moreTopPadding>また、明大祭や明大祭実行委員会の写真や映像などを使用される場合も同様に、下記のお問い合わせ先までご連絡いただきますようお願い申し上げます。</Text>
+        <Text moreTopPadding>※明大祭当日の取材につきましては、別途事前申請が必要となりますので、その旨をお伝えください。</Text>
         <ContactView department="広報局" mail="141st-kouhou@meidaisai.jp" showAddress/>
       </PageContainer>
     </div>
