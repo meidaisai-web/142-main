d264390a9e026c4357a92ad49eb138b826c65f11
tiger121005 <itiger4649@gmail.com>
2025-09-11 11:46:12 +0900

    Textコンポーネントで、上にさらに隙間を入れられる引数を追加

--- src/components/texts/Text.tsx
+++ src/components/texts/Text.tsx
@@ -2,11 +2,12 @@ type TextProps = {
     children: React.ReactNode;
     className?: string;
     noTopPadding?: boolean;
+    moreTopPadding?: boolean;
 };
 
-export default function Text({ children, noTopPadding, className }: TextProps) {
+export default function Text({ children, noTopPadding, className, moreTopPadding }: TextProps) {
     return (
-        <p className={`w-full text-justify ${noTopPadding ? "pt-0" : "pt-2"} ${className}`}>
+        <p className={`w-full text-justify ${noTopPadding ? "pt-0" : "pt-2"} ${moreTopPadding ? "mt-5" : ""} ${className}`}>
             {children}
         </p>
     )
