bb1ceaf37a6114451c1fa3c76545a99bfcc0c90c
NaYRa-gif <romuandedamame0606@gmail.com>
2025-09-12 18:02:22 +0900

    moreTopPaddingなどの修正

--- src/components/movie.tsx
+++ src/components/movie.tsx
@@ -6,15 +6,15 @@ type MovieProps = {
 
 export default function Movie ({
     href
-}: MovieProps) { 
+}: MovieProps) {
     return (
         <div className="flex justify-center ml-5 mr-5">
-            <iframe 
-                width="560" height="315" 
-                src={href} 
-                title="YouTube video player" 
+            <iframe
+                width="560" height="315"
+                src={href}
+                title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                className="w-[80vw] h-[30vh] sm:w-[70vh] sm:h-[50vh]"
+                className="mt-5 mx-10 w-[80vw] h-[30vh] sm:w-[70vh] sm:h-[50vh]"
             >
             </iframe>
         </div>
