a31909ea5c172a62fed3d712c3eb972efc3b17ff
tiger121005 <itiger4649@gmail.com>
2025-09-13 11:06:50 +0900

    ボタンのホバー時の色を変更

--- src/components/buttons/SecondaryButton.tsx
+++ src/components/buttons/SecondaryButton.tsx
@@ -11,11 +11,11 @@ type SecondaryButtonProps = {
 export default function SecondaryButton({ className, children, href }: SecondaryButtonProps) {
     return (
         <div className={`${className} relative p-5 h-24`}>
-            <Link href={href} className="absolute -translate-x-1/2 left-1/2 whitespace-nowrap">
-                <div className='-rotate-3 rounded-full border-4 border-secondary py-3 px-20 sm:px-30 text-center absolute -translate-x-1/2 left-1/2'>
+            <Link href={href} className="absolute -translate-x-1/2 left-1/2 whitespace-nowrap group">
+                <div className='-rotate-3 rounded-full border-4 group-hover:bg-secondary border-secondary py-3 px-20 sm:px-30 text-center absolute -translate-x-1/2 left-1/2 transition duration-100'>
                     <p className='opacity-0'>{children}</p>
                 </div>
-                <div className='font-bold rounded-full hover:bg-secondary transition duration-100 border-4 border-accent py-3 px-20 sm:px-30 absolute z-10 -translate-x-1/2 left-1/2'>
+                <div className='font-bold rounded-full transition duration-100 border-4 border-accent py-3 px-20 sm:px-30 absolute z-10 -translate-x-1/2 left-1/2'>
                     {children}
                 </div>
             </Link>
