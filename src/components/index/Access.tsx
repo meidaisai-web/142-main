f0d9d58cda3302d9ca23deb0be5f24b504c517f3
da10nagi <meiji.dai1math@gmail.com>
2025-09-13 18:10:42 +0900

    角の丸みを修正

--- src/components/index/Access.tsx
+++ src/components/index/Access.tsx
@@ -6,7 +6,7 @@ const Access = () => {
         <div>
             <IndexTitle>アクセス</IndexTitle>
             <div className="mt-10 text-center text-xl font-bold">京王線・京王井の頭線「明大前駅」より徒歩5分</div>
-            <div className="mt-10 w-3/4 lg:w-5/12 mx-auto rounded-4xl overflow-hidden">
+            <div className="mt-10 w-3/4 lg:w-5/12 mx-auto rounded-3xl overflow-hidden">
                 <Image src="/images/ad/accessmap.jpg" alt="アクセスマップ" width={400} height={400} className="w-full h-full" />
             </div>
         </div>
