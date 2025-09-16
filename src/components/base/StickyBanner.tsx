a72d8c404c9af817990e26b75524b5a713adaca1
tiger121005 <itiger4649@gmail.com>
2025-09-16 22:52:22 +0900

    stickyBannerにもadDataのid追加

--- src/components/base/StickyBanner.tsx
+++ src/components/base/StickyBanner.tsx
@@ -27,7 +27,7 @@ export default function StickyBanner({ adData }: StickyBannerProps) {
     return (
         <div className="fixed bottom-0 md:hidden w-screen max-w-[375px] z-40 object-contain flex justify-center">
             <Link id='sticky_banner' href={adData.url} target='_blank'>
-                <Image src={adData.src} alt="logo" width={375} height={83} className='object-contain' />
+                <Image id={adData.id} src={adData.src} alt="logo" width={375} height={83} className='object-contain' />
             </Link>
         </div>
     );
