1caa87dc2a7b446fb0f03c33f7164bc0238db1d3
NaYRa-gif <romuandedamame0606@gmail.com>
2025-09-21 15:55:45 +0900

    faqページ完成

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -3,9 +3,99 @@ import PageTitle from "@/components/texts/PageTitle";
 import Text from "@/components/texts/Text";
 import PageContainer from "@/components/base/PageContainer";
 import { FAQAccordion } from "@/components/texts/FAQAccordion";
+import Link from "next/link";
+import { div } from "framer-motion/client";
 
 export default function Page() {
     return (
-        <PageTitle>よくある質問</PageTitle>
+        <div className="bg-primary">
+            <PageTitle className="mb-5">よくある質問</PageTitle>
+            <FAQAccordion question="明大祭の開催時間は何時から何時までですか。">
+                会場が10:00、開催時間が11:00~18:00です。
+            </FAQAccordion>
+            <FAQAccordion question="チケットは予約は必要ですか。">
+                今年度は必要ありません。
+            </FAQAccordion>
+            <FAQAccordion question="企画の入場制限はありますか。">
+                企画によっては入場制限を設ける場合がございます｡詳しくは､こちらの<Link href="/search">｢企画を探す｣</Link>ページよりご確認ください｡
+            </FAQAccordion>
+            <FAQAccordion question="雨天時でも明大祭は開催しますか。">
+                開催いたします｡ただし､豪雨の際などは中止となる企画がある場合がございます｡また､台風や災害の恐れがある際は開催が中止となる場合がございます｡その際は第140回明大祭公式サイトおよび各種公式SNSでお知らせをする予定です｡
+            </FAQAccordion>
+            <FAQAccordion question="新型コロナウイルス感染症対策は必要ですか。">
+                必須ではありません｡マスクの着用などの感染症対策は個人に委ねております｡
+            </FAQAccordion>
+            <FAQAccordion question="公式パンフレットはもらえますか。">
+                インフォメーションブースにて配布しております｡インフォメーションブースの場所は､ <Link href="/map">こちらのページ</Link>にてキャンパスマップをご確認ください｡
+            </FAQAccordion>
+            <FAQAccordion question="大学構内にATMはありますか。">
+                大学構内にあるATMは以下のとおりです｡<br />
+                <table>
+                    <thead>
+                        <tr className="border">
+                            <th className="pr-4 text-center">金融機関名<br />（設置場所）</th>
+                            <th className="pr-4 text-center">日付</th>
+                            <th className="pr-4 text-center">稼働時間</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="text-center">
+                                <span>三井住友銀行<br /></span>
+                                <span>(第一校舎1階)</span>
+                            </td>
+                            <td className="pl-4 text-left">11月1日(土)</td>
+                            <td className="pl-4 text-left">9:00~13:00</td>
+                        </tr>
+                        <tr>
+                            <td className="pl-4 text-left">11月2日(日)</td>
+                            <td className="pl-4 text-left">休止</td>
+                        </tr>
+                        <tr>
+                            <td className="pl-4">11月3日(月・祝)</td>
+                            <td className="">休止</td>
+                        </tr>
+
+                        <tr>
+                            <td className="text-center">
+                                <span>ゆうちょ銀行<br /></span>
+                                <span>(メディア棟1階)</span>
+                            </td>
+                            <td className="pl-4 text-left">11月1日(土)</td>
+                            <td className="pl-4 text-left">9:00~17:00</td>
+                        </tr>
+                        <tr>
+                            <td className="pl-4 text-left">11月2日(日)</td>
+                            <td className="pl-4 text-left">休止</td>
+                        </tr>
+                        <tr>
+                            <td className="pl-4">11月3日(月・祝)</td>
+                            <td className="">休止</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </FAQAccordion>
+            <FAQAccordion question="入試案内はどこにありますか。">
+                大学ガイドブックは正門付近の守衛所脇にご用意しております｡ご自由にお取りください｡また､第140回明大祭では受験生応援企画を行っています｡詳しくは<Link href="/candidate">こちらのページ</Link>をご覧ください｡
+            </FAQAccordion>
+            <FAQAccordion question="キャンパス内で飲食は可能ですか。">
+                可能です｡休憩所がございますので､飲食の際はそちらをご利用ください｡休憩所の場所は､ <Link href="map">こちらのページ</Link>にてキャンパスマップをご確認ください｡
+            </FAQAccordion>
+            <FAQAccordion question="食堂は営業していますか。">
+                明大祭期間中､食堂は1階のみ11時から14時に営業しております｡営業終了後は休憩所としてご利用いただけます｡
+            </FAQAccordion>
+            <FAQAccordion question="コンビニエンスストアはありますか。">
+                キャンパス内に｢ファミリーマート明大マート和泉店｣がございます｡ただし､商品数を減らして営業している場合がございますので､予めご了承ください。
+            </FAQAccordion>
+            <FAQAccordion question="駐車場はありますか。">
+                ご用意しておりません｡電車やバスなどの公共交通機関でのご来場をお願いしております｡
+            </FAQAccordion>
+            <FAQAccordion question="飲酒は可能ですか。">
+                大学構内はノンアルコール飲料を含め､酒類の持ち込み・飲酒行為は禁止しております｡ご了承ください｡
+            </FAQAccordion>
+            <FAQAccordion question="ペットの入構は可能ですか。">
+                申し訳ございませんが､ペットを連れての入場はご遠慮いただいております｡なお､補助犬については同伴可能です｡
+            </FAQAccordion>
+        </div>
     )
 }
--- src/components/texts/FAQAccordion.tsx
+++ src/components/texts/FAQAccordion.tsx
@@ -2,10 +2,6 @@
 
 import {ReactNode, useState} from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import HamburgerTitle from "@/components/base/Hamburger/HamburgerTitle";
-import Image from "next/image";
-import { div } from "framer-motion/client";
-
 
 interface AccordionProps {
     children: ReactNode;
@@ -15,44 +11,45 @@ interface AccordionProps {
 export function FAQAccordion({ children, question }: AccordionProps) {
     const [isOpen, setOpen] = useState<boolean>(false);
     return (
-        <div className="flex flex-col items-center font-body pt-5">
+        <div className="flex flex-col items-center justify-center font-body pt-5">
             <div role="grounp">
                 <button
                     type="button"
                     aria-controls="contents"
                     aria-expanded={isOpen}
                     onClick={() => setOpen(!isOpen)}
-                    className="px-5 flex justify-between items-center gap-3 rounded-lg shadow-[4px_3px_#64BDEA] bg-white"
+                    className="flex justify-center items-center rounded-xl"
                 >
-                    <div className="flex justify-start items-center text-left">
+                    <div className="flex pl-5 items-center text-left bg-white rounded-xl border-secondary border-4 w-[80vw] lg:w-[60vw] gap-3">
                         <div className="text-2xl font-bold text-accent">Q.</div>
-                        <div className="text-base p-5 sm:pr-0 lg:pr-10">{question}</div>
+                        <div className="text-base text-black font-bold p-5 sm:pr-0 lg:pr-10 flex-1">{question}</div>
+                        <div className="pr-5">
+                            <AnimatePresence>
+                                {isOpen ? (
+                                    <motion.div
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
+                                        exit={{ opacity: 0 }}
+                                        transition={{ duration: 0.25 }}
+                                    >
+                                        <div className="text-2xl text-accent">-</div>
+                                    </motion.div>
+                                ) : (
+                                    <motion.div
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
+                                        exit={{ opacity: 0 }}
+                                        transition={{ duration: 0.25 }}
+                                    >
+                                        <div className="text-2xl text-accent">+</div>
+                                    </motion.div>
+                                )}
+
+                            </AnimatePresence>
+                        </div>
                     </div>
-
-                    <AnimatePresence>
-                        {isOpen ? (
-                            <motion.div
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 0.25 }}
-                            >
-                                <div className="text-2xl font-mediium text-accent">-</div>
-                            </motion.div>
-                        ) : (
-                            <motion.div
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 0.25 }}
-                            >
-                                <div className="text-2xl font-mediium text-accent">+</div>
-                            </motion.div>
-                        )}
-
-                    </AnimatePresence>
                 </button>
-                <div>
+                <div className="flex items-center justify-center">
                     <AnimatePresence>
                         {isOpen && (
                             <motion.div
@@ -61,9 +58,9 @@ export function FAQAccordion({ children, question }: AccordionProps) {
                                 exit={{ opacity: 0, height: 0 }}
                                 transition={{ duration: 0.25 }}
                             >
-                                <div className="flex justify-start items-center text-left bg-white rounded-xl border-secondary border-4 mt-6 sm:w-[80vw] lg:w-[60vw]">
+                                <div className="flex justify-center items-center text-left bg-white rounded-xl border-secondary border-4 mt-6 w-[75vw] lg:w-[55vw]">
                                     <div className="text-2xl font-bold text-secondary pl-5">A.</div>
-                                    <div className="w-full text-base p-5">{children}</div>
+                                    <div className="w-full text-base text-black font-semibold p-5">{children}</div>
                                 </div>
                             </motion.div>
                         )}
