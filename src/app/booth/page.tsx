58901e21c818556007e14acf7eb359f1e431be8a
tiger121005 <itiger4649@gmail.com>
2025-09-30 15:59:42 +0900

    画像修正

--- src/app/booth/page.tsx
+++ src/app/booth/page.tsx
@@ -1,11 +1,11 @@
 import AccentText from "@/components/texts/AccentText";
 import PageTitle from "@/components/texts/PageTitle";
 import PageContainer from "@/components/base/PageContainer";
-import Image from "next/image";
 import Text from "@/components/texts/Text";
 import SectionTitle from "@/components/texts/SectionTitle";
 import SmallTitle from "@/components/texts/SmallTitle";
 import ContactView from "@/components/texts/ContactView";
+import OnlyImage from "@/components/OnlyImage";
 
 
 export default function Page() {
@@ -26,13 +26,7 @@ export default function Page() {
                 <Text>11月1日(土).2日(日).3日(月・祝) 10：00〜17：30</Text>
                 <AccentText>実施場所</AccentText>
                 <Text>和泉図書館横</Text>
-                <Image
-                src="/images/booth-location.jpg"
-                alt="和泉図書館横"
-                width={800}
-                height={600}
-                className="my-5 rounded-lg w-[80vw] sm:w-[60vw] md:w-[40vw] lg:w-[30vw] xl:w-[25vw] 2xl:w-[20vw] "
-                />
+                <OnlyImage src="/images/booth-location.jpg" alt="和泉図書館横" className="max-w-72" />
                 <SectionTitle className="mb-5">出展企業一覧</SectionTitle>
                 {data.map((item) => (
                     <div key={item.name} className="mb-5">
