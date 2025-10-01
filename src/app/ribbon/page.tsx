fd1920ad4fa32a6786ea65b2edf92dbd3505429e
Ena Sakai <enamar2807@gmail.com>
2025-10-01 11:24:58 +0900

    Re:Bond～リボンでつながる思い出～

---
+++ src/app/ribbon/page.tsx
@@ -0,0 +1,39 @@
+import PageTitle from "@/components/texts/PageTitle"
+import SectionTitle from "@/components/texts/SectionTitle"
+import Text from "@/components/texts/Text"
+import PageContainer from "@/components/base/PageContainer"
+import Emphasis from "@/components/texts/Emphasis"
+import SmallTitle from "@/components/texts/SmallTitle"
+export default function Page() {
+    return (
+        <>
+            <PageTitle>Re:Bond～リボンでつながる思い出～</PageTitle>
+            <PageContainer>
+                <SectionTitle>企画概要</SectionTitle>
+                <Text moreTopPadding>
+                    <Emphasis>明大祭での思い出を、自分だけの形に残そう！</Emphasis>
+                </Text>
+                <Text>
+                    第141回明大祭公式テーマカラーである焔紅、蒼炎、軌光の3色のリボンを選んで、スタンプなどでデコレーションすると
+                    <Emphasis>自分だけのオリジナルキーホルダーが作れる！</Emphasis>
+                </Text>
+                <Text>
+                    作ったキーホルダーは持ち帰ることができます！みんなでバッグや鍵につけて、明大祭を思い出とともに楽しもう!!
+                </Text>
+                <Text>
+                    キーホルダー制作時に残ったリボンを使って、みんなで1つの絵を完成させよう！どんな絵ができるかは参加してからのお楽しみ!!
+                </Text>
+                <Text>
+                    お子様連れでも楽しめるので、是非ぜひ第一校舎前にお越しください！
+                </Text>
+                <SectionTitle>企画実施日時・場所</SectionTitle>
+                <SmallTitle>日時</SmallTitle>
+                <Text moreTopPadding>
+                    11月1日(土).2日(日).3日(月・祝)10:00~18:00 （最終日のみ17：00まで）
+                </Text>
+                <SmallTitle className="mt-8">場所</SmallTitle>
+                <Text moreTopPadding>第一校舎前正面左側</Text>
+            </PageContainer>
+        </>
+    )
+}
