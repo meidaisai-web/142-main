5bd00d532e5091aa9748baa9ad768c6bd5428fce
NaYRa-gif <romuandedamame0606@gmail.com>
2025-10-06 14:06:59 +0900

    MapImageの追加と一部文章の変更

--- src/app/illumination/page.tsx
+++ src/app/illumination/page.tsx
@@ -3,6 +3,7 @@ import PageContainer from "@/components/base/PageContainer"
 import Text from "@/components/texts/Text"
 import SectionTitle from "@/components/texts/SectionTitle"
 import SmallTitle from "@/components/texts/SmallTitle"
+import MapImage from "@/components/MapImage"
 import Image from "next/image"
 
 
@@ -11,24 +12,16 @@ export default function Page() {
         <div>
             <PageTitle>明大祭イルミネーション</PageTitle>
             <PageContainer>
-                <SectionTitle className="mb-10">熱狂を包む、余韻の光</SectionTitle>
-                <Text>
-                    明大祭の夜を、色鮮やかなイルミネーションが照らします。 <br />
-                    光の中で、熱狂の余韻に存分に浸ってみませんか？ <br />
-                    忘れられないひとときを、あなたにお届けします。
-                </Text>
+                <SectionTitle>熱狂を包む、余韻の光</SectionTitle>
+                <Text moreTopPadding>明大祭の夜を、色鮮やかなイルミネーションが照らします。</Text>
+                <Text>光の中で、熱狂の余韻に存分に浸ってみませんか？</Text>
+                <Text>忘れられないひとときを、あなたにお届けします。</Text>
                 <SectionTitle className="mb-10">企画実施日時</SectionTitle>
-                <SmallTitle>【日時】</SmallTitle>
+                <SmallTitle>日時</SmallTitle>
                 <Text>11月1日(土).2日(日).3日(月・祝)16：00〜</Text>
-                <SmallTitle>【場所】</SmallTitle>
+                <SmallTitle>場所</SmallTitle>
                 <Text>和泉キャンパス正門付近</Text>
-                <Image
-                    src="/images/svg/illumination-location.svg"
-                    alt="和泉キャンパス正門付近"
-                    width={800}
-                    height={600}
-                    className="my-10 rounded-lg w-[30rem]"
-                />
+                <MapImage src="/images/map/illumination.jpg" alt="和泉キャンパス正門付近"/>
             </PageContainer>
         </div>
     )
