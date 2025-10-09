8d919aeb776b1b81eddf31948609110efca755ff
tiger121005 <itiger4649@gmail.com>
2025-10-09 16:09:57 +0900

    引き換え画面整頓

--- src/components/voucher/Voucher.tsx
+++ src/components/voucher/Voucher.tsx
@@ -1,38 +1,37 @@
 'use client';
 
-import { exchange, isAlreadyExchange, isInTime } from "@/utils/managers/voucherManager"
+import { exchange, isAlreadyExchange } from "@/utils/managers/voucherManager"
 import { useEffect, useState } from "react"
 import Button from "../buttons/Button";
 import { hasVotedToday } from "@/utils/managers/meichamManager";
-import Text from "../texts/Text";
 
 
 export default function Voucher() {
-    const [error, setError] = useState<string>("");
+    const [error, setError] = useState<string | null>(null);
     const [disabled, setDisabled] = useState(true);
-    const [buttonText, setButtonText] = useState("引換不可");
+    const [buttonText, setButtonText] = useState("引き換え不可");
 
     function isEnableExchange(): boolean {
-        if (!isInTime()) {
-            setError('引換可能期間外です。')
-            setButtonText("引換不可")
-            setDisabled(true);
-            return false;
-        }
+        // if (!isInTime()) {
+        //     setError('引換可能期間外です。')
+        //     setButtonText("引換不可")
+        //     setDisabled(true);
+        //     return false;
+        // }
         if (!hasVotedToday()) {
-            setError('本日投票していないため、まだ引換できません。')
-            setButtonText("引換不可")
+            setError('本日投票していないため、まだ引き換えできません。')
+            setButtonText("引き換え不可")
             setDisabled(true);
             return false;
         }
         if (isAlreadyExchange()) {
-            setError('すでに引換済みです。')
-            setButtonText("引換済み")
+            setError(null)
+            setButtonText("引き換え済み")
             setDisabled(true);
             return false;
         }
-        setError("");
-        setButtonText("引換")
+        setError(null);
+        setButtonText("引き換え")
         setDisabled(false);
         return true;
     }
@@ -48,15 +47,29 @@ export default function Voucher() {
             return;
         }
         exchange();
-        setError("引換が完了しました。")
-        setButtonText("引換済み")
+        setError("引き換えが完了しました。")
+        setButtonText("引き換え済み")
         setDisabled(true);
     }
 
     return (
-        <div>
-            <Button onClick={handleExchange} disabled={disabled}>{buttonText}</Button>
-            <Text>{error}</Text>
+        <div className="relative mx-auto w-lg max-w-full my-16">
+            <div className="absolute rotate-3 -z-10 mx-auto py-10 rounded-2xl border-accent border-4 w-full text-primary px-10">
+                <p className="font-bold text-xl text-center">抽選券への引き換えはこちら！</p>
+                <p className="text-primary font-semibold mt-3">必ず総合インフォメーションで、抽選券引き換え時にタップしてください。</p>
+                <Button onClick={handleExchange} disabled={disabled} className="mt-3">{buttonText}</Button>
+                {error &&
+                    <p className="text-center font-bold text-primary mt-5">※{error}</p>
+                }
+            </div>
+            <div className="mx-auto bg-white py-10 rounded-2xl border-secondary border-4 w-full text-black px-10">
+                <p className="font-bold text-xl text-center">抽選券への引き換えはこちら！</p>
+                <p className="text-primary font-semibold mt-3">必ず総合インフォメーションで、抽選券引き換え時にタップしてください。</p>
+                <Button onClick={handleExchange} disabled={disabled} className="mt-3">{buttonText}</Button>
+                {error &&
+                    <p className="text-center font-bold text-primary mt-5">※{error}</p>
+                }
+            </div>
         </div>
     )
 }
