495bcc1949a12df44c99d39d02a0da21387954db
tiger121005 <itiger4649@gmail.com>
2025-10-10 12:45:14 +0900

    投票時にアラートが出るようにした

--- src/components/search/DetailEventContent.tsx
+++ src/components/search/DetailEventContent.tsx
@@ -62,7 +62,7 @@ export default function DetailEventContent({ id }: DetailContentProps) {
                         <InfoView group={data.groupName} date={data.eventDate} location={data.location} catchphrase={data.catchphrase} detail={data.eventContent} icons={data.icons} instagram={data.instagramAccount} x={data.xAccount} youtube={data.youtubeAccount} tiktok={data.tiktokAccount} homepage={data.homepageUrl} />
                     </div>
                     <Menu menus={data.menuItems || []} />
-                    <VoteView id={id} groupId={data.groupId} type={data.type} eventDate={data.eventDate} />
+                    <VoteView id={id} groupId={data.groupId} type={data.type} eventName={data.eventName} groupName={data.groupName} eventDate={data.eventDate} />
                 </div>
             )}
         </div>
