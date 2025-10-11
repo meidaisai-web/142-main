a8ceb6fffd43e65ee5bca1dc64bf6d60356de648
Ena Sakai <enamar2807@gmail.com>
2025-10-12 00:03:06 +0900

    明大祭公式グッズ

--- src/app/goods/page.tsx
+++ src/app/goods/page.tsx
@@ -5,7 +5,8 @@ import { List, ListItem, ListText } from '@/components/texts/List'
 import PageContainer from "@/components/base/PageContainer"
 import ContactView from "@/components/texts/ContactView"
 import Emphasis from "@/components/texts/Emphasis"
-import Image from 'next/image'
+import OnlyImage from 'next/image'
+import SmallTitle from "@/components/texts/SmallTitle"
 export default function Page() {
     return (
         <>
@@ -16,82 +17,82 @@ export default function Page() {
                 <Text className="mt-10">
                     ガチャガチャ　缶バッジ（全6種内1種シークレット）　1回100円
                 </Text>
-                <Image
-                    src="/images/badge.jpg"
+                <OnlyImage
+                    src="/images/goods_badge.jpg"
                     alt="めいじろう缶バッジ一覧_01"
                     width={1700}
                     height={500}
-                    className="w-full max-w-lg mt-5 mb-15"
+                    className="w-full max-w-lg mt-4 mb-15"
                 />
                 <Text> ボールペン　250円</Text>
-                <Image
-                    src="/images/pen.jpg"
+                <OnlyImage
+                    src="/images/goods_pen.jpg"
                     alt="ボールペン_02"
                     width={1700}
                     height={500}
-                    className="w-full max-w-lg"
+                    className="w-full max-w-lg mt-4 mb-15"
                 />
                 <Text>ラバーバンド　各200円</Text>
-                <Image
-                    src="/images/rubber band.jpg"
+                <OnlyImage
+                    src="/images/goods_band.jpg"
                     alt="ラバーバンド一覧_03"
                     width={1700}
                     height={500}
-                    className="w-full max-w-2xl mt-5 mb-15"
+                    className="w-full max-w-lg mt-4 mb-15"
                 />
                 <Text>クリアファイル　各200円</Text>
-                <Image
-                    src="/images/file1.jpg"
-                    alt="めいじろうクリアファイル(蒼炎)_04"
+                <OnlyImage
+                    src="/images/goods_file3.jpg"
+                    alt="めいじろうクリアファイル(パンフレット表紙)_04"
                     width={1700}
                     height={500}
-                    className="w-full max-w-lg mt-5"
+                    className="w-full max-w-lg mt-4"
                 />
-                <Image
-                    src="/images/file2.jpg"
-                    alt="めいじろうクリアファイル(軌光)_05"
+                <OnlyImage
+                    src="/images/goods_file1.jpg"
+                    alt="めいじろうクリアファイル(蒼炎)_05"
                     width={1700}
                     height={500}
-                    className="w-full max-w-lg mt-10 mb-15"
+                    className="w-full max-w-lg my-10"
                 />
-                <Image
-                    src="/images/file3.jpg"
-                    alt="めいじろうクリアファイル(蒼炎)_04"
+                <OnlyImage
+                    src="/images/goods_file2.jpg"
+                    alt="めいじろうクリアファイル(軌光)_06"
                     width={1700}
                     height={500}
-                    className="w-full max-w-lg mt-5"
+                    className="w-full max-w-lg mb-15"
                 />
                 <Text>アクリルキーホルダー　350円</Text>
-                <Image
-                    src="/images/acrylic.jpg"
+                <OnlyImage
+                    src="/images/goods_acrylic.jpg"
                     alt="めいじろうアクリルキーホルダー_07"
                     width={1700}
                     height={500}
-                    className="w-full max-w-sm mt-4 mb-15"
+                    className="w-full max-w-xs mt-4 mb-15"
                 />
                 <Text>絵馬型キーホルダー　350円</Text>
-                <Image
-                    src="/images/ema.jpg"
+                <OnlyImage
+                    src="/images/goods_ema.jpg"
                     alt="絵馬型キーホルダー_08"
                     width={1700}
                     height={500}
-                    className="w-full max-w-sm mt-4 mb-15"
+                    className="w-full max-w-xs mt-4 mb-15"
                 />
                 <Text>※画像はイメージです。</Text>
                 <SectionTitle>販売詳細</SectionTitle>
-                <Text moreTopPadding>【販売日時】</Text>
+                <SmallTitle>販売日時</SmallTitle>
                 <Text>2025年11月1日(土).2日(日).3日(月・祝)10：00～18：00（3日間共通）</Text>
-                <Text>【販売場所】</Text>
+                <SmallTitle>販売場所</SmallTitle>
                 <Text>明治大学和泉キャンパス明大祭公式グッズ販売ブース（和泉図書館前）</Text>
                 <SectionTitle>注意事項</SectionTitle>
                 <List className="mt-8" mark="・">
                     <ListItem>お支払いは<Emphasis>現金・キャッシュレス</Emphasis>に対応しております。</ListItem>
-                    <Image
-                        src="/images/payment.jpg"
+                    <OnlyImage
+                        src="/images/goods_payment.jpg"
                         alt="キャッシュレス決済一覧_09"
                         width={1700}
                         height={500}
-                        className="w-full max-w-xs mt-5 mb-15"
+                        className="w-full max-w-xs mt-4 mb-10"
                     />
                     <ListItem>商品がなくなりしだい終了となります。</ListItem>
                     <ListItem>不良品以外返品・交換はできません。</ListItem>
