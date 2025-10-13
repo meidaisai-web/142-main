e8865b3c9ac6e242b92246578b6b300d7e016511
NaYRa-gif <romuandedamame0606@gmail.com>
2025-10-13 14:16:54 +0900

    強調分の追加

--- src/app/bazaar/page.tsx
+++ src/app/bazaar/page.tsx
@@ -5,6 +5,7 @@ import Text from "@/components/texts/Text"
 import SmallTitle from "@/components/texts/SmallTitle"
 import AccentText from "@/components/texts/AccentText"
 import MapImage from "@/components/MapImage"
+import Emphasis from "@/components/texts/Emphasis"
 
 import { List } from "@/components/texts/List"
 import { ListItem } from "@/components/texts/List"
@@ -16,7 +17,7 @@ export default function Page(){
             <PageContainer>
                 <SectionTitle>実行委員会によるバザー企画開催！</SectionTitle>
                 <Text moreTopPadding>学生が持ち寄った本や服など、様々なアイテムが勢ぞろい！この機会にあなただけの特別な一品を見つけに来ませんか？</Text>
-                <Text>バザーの収益金は、災害に見舞われた方に対する救援活動や復興支援のために、<span className="font-bold">日本赤十字社に寄付</span>されます。</Text>
+                <Text>バザーの収益金は、災害に見舞われた方に対する救援活動や復興支援のために、<Emphasis>日本赤十字社に寄付</Emphasis>されます。</Text>
                 <List mark="※">
                     <ListItem>売り切れ次第終了となります。</ListItem>
                     <ListItem>最終募金金額については、明大祭終了後にこちらのページにてご報告させていただきます。</ListItem>
