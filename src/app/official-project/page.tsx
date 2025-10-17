5aa38ce429147261e2e41fa39ca29b52cbe9cede
tiger121005 <itiger4649@gmail.com>
2025-10-17 19:25:42 +0900

    コンポーネントの位置変更

--- src/app/official-project/page.tsx
+++ src/app/official-project/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { Fragment, useEffect, useMemo, useRef, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import type { Swiper as SwiperType } from 'swiper';
 import TabButton from '@/components/buttons/TabButton';
 import PageTitle from '@/components/texts/PageTitle';
 import PageContainer from '@/components/base/PageContainer';
-import Project from '@/components/project';
+import Link from 'next/link';
+import Image from 'next/image';
 
 type Tab = { key: string; label: string; content: React.ReactNode };
 
@@ -530,5 +531,42 @@ export default function TabsSwipe() {
         </Swiper>
       </PageContainer>
     </div>
+  );
+}
+
+type ProjectProps = {
+  title: string;
+  description: string;
+  image: string;
+  alt: string;
+  tags: string[];
+  link: string;
+}
+
+function Project({ title, description, image, alt, tags, link }: ProjectProps) {
+  return (
+    <Link
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="block transition duration-200 rounded-2xl"
+    >
+      <div className="bg-white rounded-2xl p-4 flex flex-row md:flex-col items-center gap-6 border-7 border-accent shadow-[10px_10px_0px_0px_#3571B8] w-full md:w-[380px] transition duration-200 transform hover:-translate-y-1 hover:-translate-x-1">
+        <div className="w-1/2 md:w-full flex justify-center">
+          <Image src={image} alt={alt} width={200} height={200} className="rounded-lg object-cover" />
+        </div>
+        <div className="flex-1 md:flex-none md:w-full flex flex-col gap-4">
+          <h2 className="text-xl text-black font-bold">{title}</h2>
+          <p className="text-base text-black">{description}</p>
+          <div className="flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <span key={tag} className="bg-secondary rounded-full px-3 py-1 text-sm font-semibold">
+                {`#${tag}`}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Link>
   );
 }
