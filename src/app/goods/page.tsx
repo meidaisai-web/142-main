11d775178119aed6a6ed88820d29cc50f9c280ed
tiger121005 <itiger4649@gmail.com>
2025-10-17 16:19:33 +0900

    リファクタ

--- src/app/goods/page.tsx
+++ src/app/goods/page.tsx
@@ -1,87 +1,41 @@
 import PageTitle from "@/components/texts/PageTitle"
 import SectionTitle from "@/components/texts/SectionTitle"
 import Text from "@/components/texts/Text"
-import { List, ListItem, ListText } from '@/components/texts/List'
+import { List, ListItem } from '@/components/texts/List'
 import PageContainer from "@/components/base/PageContainer"
 import ContactView from "@/components/texts/ContactView"
 import Emphasis from "@/components/texts/Emphasis"
-import OnlyImage from 'next/image'
+import OnlyImage from "@/components/OnlyImage"
 import SmallTitle from "@/components/texts/SmallTitle"
+
 export default function Page() {
+    const goodsList = [
+        { name: 'ガチャガチャ　缶バッジ（全6種内1種シークレット）', price: '1回100円', src: ['/images/goods/badge.jpg'] },
+        { name: 'ボールペン', price: '250円', src: ['/images/goods/pen.jpg'] },
+        { name: 'ラバーバンド', price: '各200円', src: ['/images/goods/band.jpg'] },
+        { name: 'クリアファイル', price: '各200円', src: ['/images/goods/file3.jpg', '/images/goods/file1.jpg', '/images/goods/file2.jpg'] },
+        { name: 'アクリルキーホルダー', price: '350円', src: ['/images/goods/acrylic.jpg'] },
+        { name: '絵馬型キーホルダー', price: '350円', src: ['/images/goods/ema.jpg'] },
+    ]
     return (
         <>
             <PageTitle>明大祭公式グッズ</PageTitle>
             <PageContainer>
-                <Text className="mt-20">昨年大好評につき、今年も明大祭公式グッズの販売が決定いたしました！ご来場記念にぜひお買い求めください！</Text>
+                <Text className="mt-10">昨年大好評につき、今年も明大祭公式グッズの販売が決定いたしました！ご来場記念にぜひお買い求めください！</Text>
                 <SectionTitle>ラインナップ</SectionTitle>
-                <Text className="mt-10">
-                    ガチャガチャ　缶バッジ（全6種内1種シークレット）　1回100円
-                </Text>
-                <OnlyImage
-                    src="/images/goods/badge.jpg"
-                    alt="めいじろう缶バッジ一覧_01"
-                    width={1700}
-                    height={500}
-                    className="w-full max-w-lg mt-4 mb-15"
-                />
-                <Text> ボールペン　250円</Text>
-                <OnlyImage
-                    src="/images/goods/pen.jpg"
-                    alt="ボールペン_02"
-                    width={1700}
-                    height={500}
-                    className="w-full max-w-lg mt-4 mb-15"
-                />
-                <Text>ラバーバンド　各200円</Text>
-                <OnlyImage
-                    src="/images/goods/band.jpg"
-                    alt="ラバーバンド一覧_03"
-                    width={1700}
-                    height={500}
-                    className="w-full max-w-lg mt-4 mb-15"
-                />
-                <Text>クリアファイル　各200円</Text>
-                <OnlyImage
-                    src="/images/goods/file3.jpg"
-                    alt="めいじろうクリアファイル(パンフレット表紙)_04"
-                    width={1700}
-                    height={500}
-                    className="w-full max-w-lg mt-4"
-                />
-                <OnlyImage
-                    src="/images/goods/file1.jpg"
-                    alt="めいじろうクリアファイル(蒼炎)_05"
-                    width={1700}
-                    height={500}
-                    className="w-full max-w-lg my-10"
-                />
-                <OnlyImage
-                    src="/images/goods/file2.jpg"
-                    alt="めいじろうクリアファイル(軌光)_06"
-                    width={1700}
-                    height={500}
-                    className="w-full max-w-lg mb-15"
-                />
-                <Text>アクリルキーホルダー　350円</Text>
-                <OnlyImage
-                    src="/images/goods/acrylic.jpg"
-                    alt="めいじろうアクリルキーホルダー_07"
-                    width={1700}
-                    height={500}
-                    className="w-full max-w-xs mt-4 mb-15"
-                />
-                <Text>絵馬型キーホルダー　350円</Text>
-                <OnlyImage
-                    src="/images/goods/ema.jpg"
-                    alt="絵馬型キーホルダー_08"
-                    width={1700}
-                    height={500}
-                    className="w-full max-w-xs mt-4 mb-15"
-                />
+                {goodsList.map((item, index) => (
+                    <div key={index}>
+                        <SmallTitle className="mt-8">{item.name}</SmallTitle>
+                        <Text>{item.price}</Text>
+                        {item.src.map((src) =>
+                            <OnlyImage key={src} src={src} alt={item.name} className="w-full max-w-lg mt-4" />
+                        )}
+                    </div>
+                ))}
                 <Text>※画像はイメージです。</Text>
                 <SectionTitle>販売詳細</SectionTitle>
                 <SmallTitle>販売日時</SmallTitle>
-                <Text>2025年11月1日(土).2日(日).3日(月・祝)10：00～18：00（3日間共通）</Text>
+                <Text>11月1日(土).2日(日).3日(月・祝)10：00～18：00</Text>
                 <SmallTitle>販売場所</SmallTitle>
                 <Text>明治大学和泉キャンパス明大祭公式グッズ販売ブース（和泉図書館前）</Text>
                 <SectionTitle>注意事項</SectionTitle>
@@ -90,15 +44,13 @@ export default function Page() {
                     <OnlyImage
                         src="/images/goods/payment.jpg"
                         alt="キャッシュレス決済一覧_09"
-                        width={1700}
-                        height={500}
                         className="w-full max-w-xs mt-4 mb-10"
                     />
                     <ListItem>商品がなくなりしだい終了となります。</ListItem>
                     <ListItem>不良品以外返品・交換はできません。</ListItem>
                     <ListItem>購入後、アンケートに回答していただいた方には、実行委員主催の抽選企画の抽選券をお渡しいたします。</ListItem>
                 </List>
-                <ContactView department="制作局　明大祭公式グッズチーム" mail="141-st goodsteam@meidaisai.jp" showPhone showAddress />
+                <ContactView department="制作局　明大祭公式グッズチーム" mail="141st-goodsteam@meidaisai.jp" showPhone showAddress />
             </PageContainer>
         </>
     )
