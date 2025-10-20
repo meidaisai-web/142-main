1e9a27b4be2b44bff009a4e467e01d58bdee0b48
tiger121005 <itiger4649@gmail.com>
2025-10-20 15:41:33 +0900

    改行なし

--- src/app/clash/page.tsx
+++ src/app/clash/page.tsx
@@ -2,7 +2,6 @@ import PageTitle from "@/components/texts/PageTitle"
 import PageContainer from "@/components/base/PageContainer"
 import Text from "@/components/texts/Text"
 import SectionTitle from "@/components/texts/SectionTitle"
-import AccentText from "@/components/texts/AccentText"
 import SmallTitle from "@/components/texts/SmallTitle"
 import OnlyImage from "@/components/OnlyImage"
 import LogoImage from "@/components/LogoImage"
@@ -25,8 +24,7 @@ export default function Page() {
                 <Text>この頂点は、間違いなく最強だ。</Text>
 
                 <SectionTitle>企画概要</SectionTitle>
-                <Text moreTopPadding>勝敗を決めるのは、あなたの１票。明大祭初夜、頂点を懸けた戦いが始まる。勝利を手にするのは、3連覇を狙う中野ダンスサークルSIGN擁するTEAM QUEENか—悲願達成を誓う</Text>
-                <Text noTopPadding>K-POPカバーダンスサークルMercie擁するTEAM KINGか。激闘の行く末に待っているものとは—その瞬間を、見逃すな。</Text>
+                <Text moreTopPadding>勝敗を決めるのは、あなたの1票。明大祭初夜、頂点を懸けた戦いが始まる。勝利を手にするのは、3連覇を狙う中野ダンスサークルSIGN擁するTEAM QUEENか—悲願達成を誓うK-POPカバーダンスサークルMercie擁するTEAM KINGか。激闘の行く末に待っているものとは—その瞬間を、見逃すな。</Text>
                 <SectionTitle>企画実施日時・場所</SectionTitle>
                 <SmallTitle>日時</SmallTitle>
                 <Text>11月1日(土) 17：10〜18：00</Text>
@@ -35,11 +33,11 @@ export default function Page() {
                 <Text>メインステージ</Text>
 
                 <SectionTitle>出演団体</SectionTitle>
-                <AccentText>TEAM QUEEN</AccentText>
+                <SmallTitle>TEAM QUEEN</SmallTitle>
                 <Text>chocolat lumière：ユニドル全国優勝経験者</Text>
                 <Text>アカペラサークルSound Arts：ハモネプ優勝経験者</Text>
                 <Text>中野ダンスサークルSIGN：前人未到の3連覇へ</Text>
-                <AccentText>TEAM KING</AccentText>
+                <SmallTitle>TEAM KING</SmallTitle>
                 <Text>SHINE：中野ダンスサークルSIGNからの刺客</Text>
                 <Text>アカペラサークルamour：音で挑み、声で勝つ。</Text>
                 <Text>K-POPカバーダンスサークルMercie：打倒・王者。悲願達成へ。</Text>
