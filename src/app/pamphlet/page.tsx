850aa9a6473b446edf51692ec8e4cbc338f90328
tiger121005 <itiger4649@gmail.com>
2025-10-29 12:24:24 +0900

    画像とURL修正

--- src/app/pamphlet/page.tsx
+++ src/app/pamphlet/page.tsx
@@ -1,9 +1,9 @@
 import PageTitle from "@/components/texts/PageTitle"
 import PageContainer from "@/components/base/PageContainer"
 import OnlyImage from "@/components/OnlyImage"
-import TransitionLink from "@/components/buttons/TransitionLink"
 import {List} from "@/components/texts/List"
 import {ListItem} from "@/components/texts/List"
+import Link from "next/link"
 
 export default function Page() {
     return (
@@ -14,9 +14,9 @@ export default function Page() {
                     <ListItem>画像をクリックすると別タブで表示されます｡</ListItem>
                 </List>
                 <div className="bg-white rounded-2xl p-4 mt-8 max-w-lg mx-auto">
-                    <TransitionLink href="">
-                        <OnlyImage src="/images/pamphlet.svg" alt="第141回明大祭公式パンフレット表紙" />
-                    </TransitionLink>
+                    <Link href="https://asset.meidaisai.jp/141-pamphlet.pdf" target="_blank">
+                        <OnlyImage src="/images/141-pamphlet-front.jpg" alt="第141回明大祭公式パンフレット表紙" />
+                    </Link>
                 </div>
             </PageContainer>
         </div>
