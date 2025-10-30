a3953eb0a9206181d3888e18d6045fa44e10bf1e
SaraHoriguchi <shalaikukou@gmail.com>
2025-10-30 16:41:24 +0900

    TT03

--- src/app/timetable/page.tsx
+++ src/app/timetable/page.tsx
@@ -9,27 +9,27 @@ export default function Page() {
             <PageContainer>
                 <SectionTitle className="mt-15 mb-10">11月1日(土)</SectionTitle>
                 <div className="flex flex-col md:flex-row justify-center items-center gap-10">
-                    <a rel="noopener noreferrer" target="_blank" className="transition-transform transform hover:scale-105 hover:shadow-lg" href="https://acrobat.adobe.com/id/urn:aaid:sc:AP:8e2b3059-a0a8-4028-ac55-efe6e39cab96">
+                    <a rel="noopener noreferrer" target="_blank" className="transition-transform transform hover:scale-105 hover:shadow-lg" href="https://asset.meidaisai.jp/141-tt/tt_1-ls.pdf">
                         <Image src='/images/timetable/TT_1-ls.jpg' alt='タイムテーブル1-1' width={500} height={300} />
                     </a>
-                    <a rel="noopener noreferrer" target="_blank" className="transition-transform transform hover:scale-105 hover:shadow-lg" href="https://acrobat.adobe.com/id/urn:aaid:sc:ap:a20a131e-cf13-4997-8e02-c3a262e7907d">
+                    <a rel="noopener noreferrer" target="_blank" className="transition-transform transform hover:scale-105 hover:shadow-lg" href="https://asset.meidaisai.jp/141-tt/tt_1-main.pdf">
                         <Image src='/images/timetable/TT_1-main.jpg' alt='タイムテーブル1-2' width={500} height={300} />
                     </a>
                 </div>
                 <SectionTitle className="mt-10 mb-10">11月2日(日)</SectionTitle>
                 <div className="flex flex-col md:flex-row justify-center items-center gap-10">
-                    <a rel="noopener noreferrer" target="_blank" className="transition-transform transform hover:scale-105 hover:shadow-lg" href="https://acrobat.adobe.com/id/urn:aaid:sc:AP:7cc6aff0-25e6-4699-b112-83e13129f6c7">
+                    <a rel="noopener noreferrer" target="_blank" className="transition-transform transform hover:scale-105 hover:shadow-lg" href="https://asset.meidaisai.jp/141-tt/tt_2-ls.pdf">
                         <Image src='/images/timetable/TT_2-ls.jpg' alt='タイムテーブル2-1' width={500} height={300} />
                     </a>
-                    <a rel="noopener noreferrer" target="_blank" className="transition-transform transform hover:scale-105 hover:shadow-lg" href="https://acrobat.adobe.com/id/urn:aaid:sc:AP:090565cf-afdd-47e7-a919-d5c8e08b60cc">
+                    <a rel="noopener noreferrer" target="_blank" className="transition-transform transform hover:scale-105 hover:shadow-lg" href="https://asset.meidaisai.jp/141-tt/tt_2-main.pdf">
                         <Image src='/images/timetable/TT_2-main.jpg' alt='タイムテーブル2-2' width={500} height={300} />
                     </a>
                 </div>
                 <SectionTitle className="mt-10 mb-10">11月3日(月・祝)</SectionTitle>
                 <div className="flex justify-center">
-                <a rel="noopener noreferrer" target="_blank" className="transition-transform transform hover:scale-105 hover:shadow-lg" href="https://acrobat.adobe.com/id/urn:aaid:sc:AP:3df71b0a-0392-4d48-a880-5fa57fcb4f4d">
-                    <Image src='/images/timetable/TT_3.jpg' alt='タイムテーブル3' width={500} height={300} />
-                </a>
+                    <a rel="noopener noreferrer" target="_blank" className="transition-transform transform hover:scale-105 hover:shadow-lg" href="https://asset.meidaisai.jp/141-tt/tt_3.pdf">
+                        <Image src='/images/timetable/TT_3.jpg' alt='タイムテーブル3' width={500} height={300} />
+                    </a>
                 </div>
             </PageContainer>
         </>
