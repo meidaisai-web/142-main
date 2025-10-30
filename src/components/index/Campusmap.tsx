44a9933e5dcbc50cd08f426e166d577e6e775c53
tiger121005 <itiger4649@gmail.com>
2025-10-30 11:56:56 +0900

    マップ入れ替え

--- src/components/index/Campusmap.tsx
+++ src/components/index/Campusmap.tsx
@@ -7,7 +7,7 @@ const Campusmap = () => {
         <div>
             <IndexTitle>キャンパスマップ</IndexTitle>
             <div className="mt-10 w-3/4 lg:w-5/12 mx-auto rounded-3xl overflow-hidden">
-                <OnlyImage src="/images/ad/campusmap.jpg" alt="キャンパスマップ" className="w-full h-full" />
+                <OnlyImage src="/images/map/campusmap.jpg" alt="キャンパスマップ" className="w-full h-full" />
                 <Button href="/map" className="mt-8">詳しく見る</Button>
             </div>
         </div>
