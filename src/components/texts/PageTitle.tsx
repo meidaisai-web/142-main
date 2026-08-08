caa15e20d9d9a9e2a7a1a2b222b7cbb470311a7f
da10nagi <meiji.dai1math@gmail.com>
2026-08-08 20:09:53 +0900

    ページタイトル作成

--- src/components/texts/PageTitle.tsx
+++ src/components/texts/PageTitle.tsx
@@ -1,14 +1,21 @@
-import { ReactNode } from "react";
+import Image from "next/image";
 
 type PageTitleProps = {
-  children: ReactNode;
+  children: string;
   className?: string;
 };
 
 export default function PageTitle({ children, className = "" }: PageTitleProps) {
   return (
-        <h1 className="">
-          {children}
-        </h1>
+        <div className="flex justify-center">
+          <div className="relative inline-block">
+            <h1 className={`bg-repeat-x bg-bottom bg-[length:24px_3px] bg-[linear-gradient(to_right,#f9a8d4_8px,transparent_0)] text-5xl font-bold text-center mt-12 pb-4 px-6 ${className}`}>
+              {children}
+            </h1>
+            <div className="absolute -right-2 -bottom-6 translate-x-full">
+              <Image src="/images/svg/airplane.svg" alt="" width={60} height={60} />
+            </div>
+          </div>
+        </div>
   );
 }
