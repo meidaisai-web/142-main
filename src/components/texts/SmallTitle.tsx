efd38bc030185b171e0fe0c7cd5556ed967f16a8
NaYRa-gif <romuandedamame0606@gmail.com>
2026-08-10 22:46:40 +0900

    01完成

--- src/components/texts/SmallTitle.tsx
+++ src/components/texts/SmallTitle.tsx
@@ -7,8 +7,17 @@ type SmallTitleProps = {
 
 export default function SmallTitle({ className, children }: SmallTitleProps) {
     return (
-            <h1 className="">
+        <div className="flex flex-raw mt-10">
+            <Image
+                src="/images/svg/titles/smallTitle.svg"
+                alt=""
+                width="40"
+                height="40"
+                className="absolute transform -translate-x-7 -translate-y-4"
+            />
+            <h2 className="">
                 {children}
-            </h1>
+            </h2>
+        </div>
     )
 }
