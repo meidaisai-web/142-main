f55462c4b17a31945fef9c7993a973c59436ccba
ShioriTajima <chiroru7215@gmail.com>
2026-08-15 00:53:38 +0900

    セクションタイトルできた

--- src/components/texts/SectionTitle.tsx
+++ src/components/texts/SectionTitle.tsx
@@ -6,10 +6,34 @@ type SectionTitleProps = {
     className?: string;
 };
 
-export default function SectionTitle({ children, className }: SectionTitleProps) {
+export default function SectionTitle({
+    children,
+    className,
+}: SectionTitleProps) {
     return (
-        <h1 className="">
-            {children}
-        </h1>
+        <div className="relative items-center justify-center w-fit mx-auto mt-10 mb-8 px-8 py-5">
+            {/* 左上の飾り */}
+            <Image
+                src="/images/svg/section-blue.svg"
+                alt="SectionTitle-blue"
+                width={55}
+                height={55}
+                className="absolute -left-1 -top-1"
+            />
+
+            {/* タイトル */}
+            <h1 className={`relative z-10 text-center text-2xl ${className ?? ""}`}>
+                {children}
+            </h1>
+
+            {/* 右下の飾り */}
+            <Image
+                src="/images/svg/section-pink.svg"
+                alt="SectionTitle-pink"
+                width={55}
+                height={55}
+                className="absolute -right-1 -bottom-1"
+            />
+        </div>
     );
 }
