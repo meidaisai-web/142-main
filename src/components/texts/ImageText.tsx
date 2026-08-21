cd938ccdb77b66df444632eddfdca66492e5ff55
yomi <itiger4649@gmail.com>
2026-08-21 14:45:05 +0900

    80% to 4/5

--- src/components/texts/ImageText.tsx
+++ src/components/texts/ImageText.tsx
@@ -15,7 +15,7 @@ export default function ImageText({
 }: ImageTextProps) {
   return (
     <div className={`flex flex-col sm:flex-row items-center sm:items-center gap-5 sm:gap-10 py-10 ${className}`}>
-      <Image src={src} alt={alt} width={500} height={500} className="rounded-lg w-[80%] max-w-lg sm:w-1/2" />
+      <Image src={src} alt={alt} width={500} height={500} className="rounded-lg w-4/5 max-w-lg sm:w-1/2" />
       <div className="w-full text-justify">{children}</div>
     </div>
   );
