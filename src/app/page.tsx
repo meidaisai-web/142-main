628caa3ee3f9afbc927e0b8bb884e2842077a182
MeguNakajima <eb260741@meiji.ac.jp>
2026-08-21 10:07:24 +0900

    全部アドした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
+import Detailbutton from "@/components/texts/Detailbutton";
 
 export default function Home() {
   return (
     <main className="bg-top-gradient">
       <h1 className="h-2000">第142回 明大祭 開発するよ</h1>
+      <Detailbutton>詳細を見る</Detailbutton>
     </main>
   );
 }
