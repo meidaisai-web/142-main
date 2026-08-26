4a992cfdf982925c4db9f7cd8c5c55debd3a94ca
da10nagi <meiji.dai1math@gmail.com>
2026-08-26 14:38:34 +0900

    accessをdevelopの状態に

--- src/app/access/page.tsx
+++ src/app/access/page.tsx
@@ -6,7 +6,6 @@ import { List, ListItem } from '@/components/texts/List';
 import SmallTitle from '@/components/texts/SmallTitle';
 import Image from 'next/image'
 import PageTitle from '@/components/texts/PageTitle';
-import ContactView from '@/components/texts/ContactView';
 
 export const metadata: Metadata = {
     title: "アクセス",
@@ -49,7 +48,6 @@ const page = () => {
                 <div className="mt-5 sm:w-3/4 lg:w-3/4 h-[75vh] mx-auto rounded-lg overflow-hidden">
                     <iframe className='w-full h-full' src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1620.646167114116!2d139.64811683886393!3d35.66980322215147!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e2!4m5!1s0x6018f3078a4992df%3A0xc09fbf5d8c170111!2z5piO5aSn5YmN6aeF44CB44CSMTU2LTAwNDMg5p2x5Lqs6YO95LiW55Sw6LC35Yy65p2-5Y6f77yS5LiB55uu77yU77yV!3m2!1d35.6684407!2d139.6504908!4m5!1s0x6018f3aa28f1e167%3A0x390585765d39611c!2z5p2x5Lqs6YO95p2J5Lim5Yy65rC456aP77yR5LiB55uu77yZ4oiS77yRIOaYjuayu-Wkp-WtpiDlkozms4njgq3jg6Pjg7Pjg5Hjgrk!3m2!1d35.671161399999995!2d139.6483178!5e0!3m2!1sja!2sjp!4v1730416884844!5m2!1sja!2sjp" width="600" height="450" loading="lazy"></iframe>
                 </div>
-                <ContactView department="アクセス担当" mail="access@meidai.ac.jp" showAddress={true} showPhone={false} />
             </PageContainer>
         </div >
     )
