91c3ae7db464c991ff9a97fcec7c0f26018b885c
SaraHoriguchi <shalaikukou@gmail.com>
2026-08-30 18:24:06 +0900

    微調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 'use client';
-import Detailbutton from "@/components/texts/Detailbutton";
 
 export default function Home() {
   return (
     <main className="bg-top-gradient">
       <h1 className="h-2000">第142回 明大祭 開発するよ</h1>
-      <Detailbutton>詳細を見る</Detailbutton>
+      <div className="h-300"></div>
     </main>
   );
 }
