8de947c5f27979b01e128376d2a6657edc4ad4e2
kyousukep <eh260216@meiji.ac.jp>
2026-08-30 18:12:36 +0900

    コンフリクト解消

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-'use client';
+import AccentTitle from '@/components/texts/AccentTitle';
 
 export default function Home() {
   return (
     <main className="bg-top-gradient">
       <h1 className="h-2000">第142回 明大祭 開発するよ</h1>
+      <AccentTitle>第142回 明大祭 開発するよ</AccentTitle>
     </main>
   );
 }
