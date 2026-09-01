afd0a8c44a395f41ef9835a5a10da6a8f64f6295
Atsuki Shioya <atsuki@192.168.40.12>
2026-09-01 23:14:34 +0900

    page.tsxから消した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 'use client';
-import Footer from "@/components/base/Footer";
 
 export default function Home() {
   return (
     <main className="bg-top-gradient">
       <h1 className="h-2000">第142回 明大祭 開発するよ</h1>
-      <Footer />
     </main>
   );
 }
