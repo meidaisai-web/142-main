b318b6824227798c04c9e3ebd8a8b6fa5a83fb5f
yomi <itiger4649@gmail.com>
2026-09-01 23:01:51 +0900

    ファイル名修正

--- src/components/texts/AccentText.tsx
+++ src/components/texts/AccentText.tsx
@@ -7,8 +7,8 @@ type AccentTextProps = {
 
 export default function AccentText({ children, className }: AccentTextProps) {
   return (
-    <div className={`-ml-5 flex items-center gap-[0.3em] ${className ?? ''}`}>
-      <div className="w-[1em] h-[1em] rounded-full bg-gradient-to-tr from-primary to-secondary shrink-0" />
+    <div className={`-ml-5 flex items-center gap-[0.3em] mt-3 ${className ?? ''}`}>
+      <div className="w-4 h-4 rounded-full bg-gradient-to-tr from-primary to-secondary shrink-0" />
       <h1 className="font-bold">{children}</h1>
     </div>
   );
