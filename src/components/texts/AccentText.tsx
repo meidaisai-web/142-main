bcffe9f8f9a41119826801559fd865c9d0cc2acf
yomi <itiger4649@gmail.com>
2026-09-02 23:32:00 +0900

    丸のサイズをひと回り大きく

--- src/components/texts/AccentText.tsx
+++ src/components/texts/AccentText.tsx
@@ -8,7 +8,7 @@ type AccentTextProps = {
 export default function AccentText({ children, className }: AccentTextProps) {
   return (
     <div className={`-ml-5 flex items-center gap-[0.3em] mt-3 ${className ?? ''}`}>
-      <div className="w-4 h-4 rounded-full bg-gradient-to-tr from-primary to-secondary shrink-0" />
+      <div className="w-5 h-5 rounded-full bg-gradient-to-tr from-primary to-secondary shrink-0" />
       <h1 className="font-bold">{children}</h1>
     </div>
   );
