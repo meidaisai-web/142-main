85782d7e77764fc0493290563a1d3d8da216e52f
yomi <itiger4649@gmail.com>
2026-09-02 23:36:02 +0900

    無駄な変更を元に戻す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import CloudPageContainer from "@/components/base/CloudPageContainer";
-import AccentText from "@/components/texts/AccentTitle";
-import PageTitle from "@/components/texts/PageTitle";
-import SectionTitle from "@/components/texts/SectionTitle";
-import SmallTitle from "@/components/texts/SmallTitle";
-import Text from "@/components/texts/Text";
+'use client';
 
 export default function Home() {
   return (
-    <CloudPageContainer>
-      <PageTitle>ページタイトル</PageTitle>
-      <SectionTitle>セクションタイトル</SectionTitle>
-      <SmallTitle>小さいタイトル</SmallTitle>
-      <AccentText>アクセントテキスト</AccentText>
-      <Text>これは本文を表示するための、テキストコンポーネントです。ここには長い文章が表示されるため、長い文章が表示されても正確かつ読みやすいように表示される必要があります。この無駄な文章は、それが本当に満たされているかを確かめるための文章です。</Text>
-    </CloudPageContainer>
+    <main className="bg-top-gradient">
+      <h1 className="h-2000">第142回 明大祭 開発するよ</h1>
+    </main>
   );
 }
