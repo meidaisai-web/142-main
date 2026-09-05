87b705c91cfc1be48c904dc6aff690e3954cc308
SaraHoriguchi <shalaikukou@gmail.com>
2026-09-05 10:15:17 +0900

    ころんけし

--- src/components/texts/Emphasis.tsx
+++ src/components/texts/Emphasis.tsx
@@ -12,7 +12,7 @@ export default function Emphasis({ children, bold = false, secondary = false }: 
             className={`${bold ? 'text-xl font-bold' : 'font-medium'}`}
             style={{
                 backgroundImage: `${secondary ? 'linear-gradient(transparent 60%, var(--color-secondary) 60%)' :'linear-gradient(transparent 60%, var(--color-accent-300) 50%)'}`,
-                backgroundSize: '100% 1.2em',:
+                backgroundSize: '100% 1.2em',
                 backgroundRepeat: 'repeat',
                 backgroundPosition: '0 0'
             }}
