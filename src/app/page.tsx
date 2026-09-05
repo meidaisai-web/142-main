69937c3b1c23b809ea3f316fbf1accacce0597c7
SaraHoriguchi <shalaikukou@gmail.com>
2026-09-05 18:54:12 +0900

    消しミス

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="bg-top-gradient">
       <h1 className="h-2000">第142回 明大祭 開発するよ</h1>
-      <div className="h-200"></div>
     </main>
   );
 }
