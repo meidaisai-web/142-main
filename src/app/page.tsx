b3325ad34dadbcf18b36ac48ae9e56957d2abcaf
MeguNakajima <eb260741@meiji.ac.jp>
2026-09-07 12:56:20 +0900

    accesとトップページを復元

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="bg-top-gradient">
       <h1 className="h-2000">第142回 明大祭 開発するよ</h1>
-      <div className="h-300"></div>
     </main>
   );
 }
+
