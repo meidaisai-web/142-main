8c1301fa912488ea5e318959599a3ffd9d761cc8
yomi <itiger4649@gmail.com>
2026-09-08 00:55:09 +0900

    page.tsxを綺麗に

--- src/components/buttons/Detailbutton.tsx
+++ src/components/buttons/Detailbutton.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import type { ReactNode } from "react";
 
 type DetailButtonProps = {
-  href?: string;
+  href: string;
   className?: string;
   children?: ReactNode;
 };
@@ -13,141 +13,34 @@ type DetailButtonProps = {
 export default function DetailButton({
   href,
   className = "",
-  children,
+  children = "詳しくはこちら",
 }: DetailButtonProps) {
-  const [isAnimated, setIsAnimated] = useState(false);
-  const [animationKey, setAnimationKey] = useState(0);
+  const [hovered, setHovered] = useState(false);
 
   return (
-    <Link
-      href={href ?? "#"}
-      className={`${className} inline-block`}
-    >
-      <div 
-        className="w-[150px]"
-        onMouseEnter={() => {
-          setIsAnimated(false);
-        
-          setAnimationKey((prev) => prev + 1);
-
-          requestAnimationFrame(() => {
-            setIsAnimated(true);
-          });
-        }}
+    <div className={`ml-auto w-fit ${className}`}>
+      <Link
+        href={href}
+        className={"inline-block"}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
       >
-        
-        <span 
-            className="
-                block
-                text-center
-                text-[15px]
-                font-medium
-                tracking-[0.04em]
-                leading-[1.2]
-                text-text
-                [text-shadow:0px_4px_3px_rgba(80,80,80,0.35)]
-                mb-[1px]
-            "
-        >
+        <div className="w-full">
+          <span className="block text-center text-sm px-5 font-medium tracking-[0.04em] leading-[1.2] text-text [text-shadow:0px_4px_3px_rgba(80,80,80,0.35)] mb-[1px]">
             {children}
-        </span>
-
-        <div 
-          key={animationKey}
-          className="relative w-[150px] h-[14px] overflow-hidden"
-        >
-
-          <div
-              className={`
-                absolute
-                left-0
-                top-[6px]
-                w-[150px]
-                h-[1px]
-                transition-transform
-                duration-[800ms]
-                ease-in-out
-                ${
-                  isAnimated
-                    ? "translate-x-[150px]"
-                    : "translate-x-0"
-                }
-              `}
-            >
-              <span 
-                className="
-                  absolute
-                  left-0
-                  top-0
-                  w-[150px]
-                  h-[1px]
-                  bg-text
-                  shadow-[0px_4px_3px_rgba(80,80,80,0.35)]
-                "
-            />
-
-            <span 
-              className="
-                absolute
-                right-0
-                top-0
-                w-[13px]
-                h-[1px]
-                bg-text
-                rotate-[40deg]
-                origin-right
-                shadow-[0px_4px_3px_rgba(80,80,80,0.35)]
-              "
-            />
-          </div>
-
-          <div
-            className={`
-              absolute 
-              left-0
-              top-[6px]
-              w-[150px] 
-              h-[1px]
-              transition-transform
-              duration-[800ms]
-              delay-[800ms]
-              ease-in-out
-              ${
-                isAnimated
-                  ? "translate-x-0"
-                  : "translate-x-[-150px]"
-              }
-            `}
-          
-          >
-            <span 
-              className="
-                absolute
-                left-0
-                top-0
-                w-[150px]
-                h-[1px]
-                bg-text
-                shadow-[0px_4px_3px_rgba(80,80,80,0.35)]
-              "
-            />
-
-            <span 
-              className="
-                absolute
-                right-0
-                top-0
-                w-[13px]
-                h-[1px]
-                bg-text
-                rotate-[40deg]
-                origin-right
-                shadow-[0px_4px_3px_rgba(80,80,80,0.35)]
-              "
-            />
+          </span>
+          <div className="relative w-full h-4 overflow-hidden" >
+            <div className={`absolute left-0 top-[6px] w-full h-[1px] ${hovered ? "transition-transform duration-500 ease-in-out translate-x-39" : "transition-none translate-x-0"}`}>
+              <span className="absolute left-0 top-0 w-full h-[1px] bg-text shadow-[0px_4px_3px_rgba(80,80,80,0.35)]" />
+              <span className="absolute right-0 top-0 w-3 h-[1px] bg-text rotate-[40deg] origin-right shadow-[0px_4px_3px_rgba(80,80,80,0.35)]" />
+            </div>
+            <div className={`absolute left-0 top-[6px] w-full h-[1px] ${hovered ? "transition-transform duration-500 delay-500 eas-in-out translate-x-0" : "transition-none -translate-x-39"}`}>
+              <span className="absolute left-0 top-0 w-full h-[1px] bg-text shadow-[0px_4px_3px_rgba(80,80,80,0.35)]" />
+              <span className="absolute right-0 top-0 w-3 h-[1px] bg-text rotate-[40deg] origin-right shadow-[0px_4px_3px_rgba(80,80,80,0.35)]" />
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
