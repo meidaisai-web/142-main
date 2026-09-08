958ad12a5bc954d238091f8ed46f9ba2a481f95c
ShioriTajima <chiroru7215@gmail.com>
2026-09-09 01:20:22 +0900

    アクセントテキストに変更

--- src/app/ippan/page.tsx
+++ src/app/ippan/page.tsx
@@ -6,6 +6,7 @@ import Emphasis from "@/components/texts/Emphasis"
 import SmallTitle from "@/components/texts/SmallTitle"
 import { List, ListItem } from "@/components/texts/List";
 import TransitionLink from "@/components/buttons/TransitionLink"
+import AccentText from "@/components/texts/AccentText"
 export default function Page() {
     return (
         <div>
@@ -20,19 +21,17 @@ export default function Page() {
                     こちらが提示するお題に対して、該当する場面を写真で送っていただきます。
                 </Text>
                 <Text>「写真を送るのはハードルが高い…」という人も大丈夫！お題の答えとなる場所の名前や説明文だけの応募もできます！こちらは該当のキャンパスに設置された投票ボックスからも投票が可能です！</Text>
-                <SmallTitle className="mt-15">お題</SmallTitle>
+                <SmallTitle>お題</SmallTitle>
                 <Text className="my-3">お題は3つご用意しています！</Text>
-                <List mark="・">
-                    <ListItem>Meiji-1グランプリ</ListItem>
-                    <Text  className="mb-5">校内で撮影した写真に面白い一言を添えて応募していただきます！皆さんの面白い回答で明治大学を沸かせませんか？</Text>
-                    <ListItem>Be.Meiji</ListItem>
-                    <Text className="mb-5">明大生の日常を集めます！お昼休みの様子、サークルの様子などなんでも大歓迎です！</Text>
-                    <ListItem>明治コレクション2026</ListItem>
-                    <Text className="mb-5">こちらでは明大生のリアルな服装を集めます！今日のあなたのコーデを教えてください！</Text>
-                </List>
+                <AccentText>Meiji-1グランプリ</AccentText>
+                <Text className="mb-5">校内で撮影した写真に面白い一言を添えて応募していただきます！皆さんの面白い回答で明治大学を沸かせませんか？</Text>
+                <AccentText>Be.Meiji</AccentText>
+                <Text className="mb-5">明大生の日常を集めます！お昼休みの様子、サークルの様子などなんでも大歓迎です！</Text>
+                <AccentText>明治コレクション2026</AccentText>
+                <Text className="mb-5">こちらでは明大生のリアルな服装を集めます！今日のあなたのコーデを教えてください！</Text>
                 <Text>あなたの日常は誰かにとって新しい発見かも。みなさんも明大祭をつくる一員になりませんか？</Text>
                 <Text>ぜひご応募・投票お待ちしています！</Text>
-                <SmallTitle className="mt-15">応募方法</SmallTitle>
+                <SmallTitle>応募方法</SmallTitle>
                 <Text>こちらの<TransitionLink targetBlank href="https://docs.google.com/forms/d/e/1FAIpQLSem4wZZRlwB0u4jZBZ6Pe-s73-R9t7Uxk-qRYOJqEoN15o97A/viewform?usp=header">Googleフォーム</TransitionLink>から応募することができます！</Text>
                 <Text>または、</Text>
                 <List mark="・">
@@ -41,9 +40,9 @@ export default function Page() {
                     <ListItem>明治大学和泉キャンパス・駿河台キャンパス・中野キャンパスの校舎内に設置された投票ボックス</ListItem>
                 </List>
                 <Text>からも応募を受け付けています！</Text>
-                <SmallTitle className="mt-15">応募期間</SmallTitle>
+                <SmallTitle>応募期間</SmallTitle>
                 <Text>9月17日(木)～10月16日(金)</Text>
-                <SmallTitle className="mt-15">応募規約</SmallTitle>
+                <SmallTitle>応募規約</SmallTitle>
                 <Text>応募作品は運営側で事前に確認し、展示の可否を判断します。<Emphasis>本人または撮影許可を得た被写体が写っている写真をご応募ください。</Emphasis></Text>
                 <Text>応募作品・個人情報等は、本企画の実施および運営に必要な範囲でのみ使用し、その他の目的で使用することはありません。企画終了後は、使用した情報を適切に破棄・削除いたします。</Text>
                 <Text className="mt-5">応募作品は、校内・明大祭での展示に適した内容のものに限ります。以下のような写真は展示対象外となる可能性があります。</Text>
