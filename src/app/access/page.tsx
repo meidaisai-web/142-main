4522522b859456f5a539d7164a643f17eb2966d3
SaraHoriguchi <shalaikukou@gmail.com>
2026-09-11 19:14:49 +0900

    画像入れた

--- src/app/access/page.tsx
+++ src/app/access/page.tsx
@@ -32,7 +32,7 @@ export default function Page() {
                     明大前駅までのアクセス
                 </SmallTitle>
                 <div className="mt-5 sm:w-3/4 lg:w-5/12 mx-auto rounded-lg overflow-hidden">
-                    <Image src="/images/map/accessmap.jpg" alt="アクセスマップ" width={400} height={400} className="w-full" />
+                    <Image src="/images/map/AccessMap.png" alt="アクセスマップ" width={400} height={400} className="w-full" />
                 </div>
                 <SmallTitle className="mt-15">
                     明大前駅から和泉キャンパスまでのアクセス
