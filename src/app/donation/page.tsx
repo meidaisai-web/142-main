44664f9e62d70f8baebf1c8e06445e0a9f6131e0
da10nagi <meiji.dai1math@gmail.com>
2026-09-14 16:43:24 +0900

    指摘修正

--- src/app/donation/page.tsx
+++ src/app/donation/page.tsx
@@ -17,9 +17,9 @@ const page = () => {
                 <SectionTitle>「明大祭・募金企画」実施します！</SectionTitle>
                 <SmallTitle className="mb-5">みなさまからのご支援が、明大祭を明るく彩ります！</SmallTitle>
                 <ImageText src="/images/donation/donation.png" alt="明大祭・募金企画" >
-                    <Text noTopPadding>日頃より、明大祭への応援・ご協力ありがとうございます。</Text>
-                    <Text moreTopPadding>この度、9月21日(月・祝)から11月9日(月)の期間で、第142回明大祭への募金企画を行います。この取り組みは明治大学と連携した期間限定募金という初の試みとなります。</Text>
-                    {/* <Text>学生の思いがこもった当企画のサイトを、ぜひ一度ご覧ください！</Text> */}
+                    日頃より、明大祭への応援・ご協力ありがとうございます。<br/>
+                    この度、9月21日(月・祝)から11月9日(月)の期間で、第142回明大祭への募金企画を行います。この取り組みは明治大学と連携した期間限定募金という初の試みとなります。
+                    {/* 学生の思いがこもった当企画のサイトを、ぜひ一度ご覧ください！ */}
                     {/* サイトURLを入れる */}
                 </ImageText>
                 <SmallTitle>あなたの想いが、明大祭を輝かせる。</SmallTitle>
@@ -30,9 +30,9 @@ const page = () => {
                 <Text moreTopPadding>目標金額は<Emphasis>「60万円」</Emphasis>です。</Text>
                 <Text moreTopPadding>みなさまからのご支援は、イルミネーション実施のために大切に活用させていただきます。また、ご支援いただいた方には、ささやかですが<Emphasis>返礼品もご用意しております。</Emphasis></Text>
                 <Text moreTopPadding>この募金企画を通じたみなさまからのご支援が、目に見える形で明大祭を彩ります。どうかお力添えをよろしくお願いいたします。</Text>
-                <List mark="※">
+                {/* <List mark="※">
                     <ListItem>詳しい情報は、上記URLまたは「明治大学　明大祭プロジェクト」で検索してご確認ください。</ListItem>
-                </List>
+                </List> */}
                 <ContactView department="第142回明大祭実行委員会 財務局　明大祭募金企画チーム" showPhone mail="142nd-bokin@meidaisai.jp" showAddress/>
             </CloudPageContainer>
         </div>
