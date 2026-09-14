5cf2408a17b7577561ed96bbd0ce87398d4c0e98
ShioriTajima <chiroru7215@gmail.com>
2026-09-14 21:58:15 +0900

    色薄くした

--- src/components/texts/Emphasis.tsx
+++ src/components/texts/Emphasis.tsx
@@ -11,7 +11,7 @@ export default function Emphasis({ children, bold = false, secondary = false }: 
         <span
             className={`${bold ? 'text-xl font-bold' : 'font-semibold'}`}
             style={{
-                backgroundImage: `${secondary ? 'linear-gradient(transparent 60%, var(--color-secondary) 60%)' :'linear-gradient(transparent 60%, var(--color-accent-300) 50%)'}`,
+                backgroundImage: `${secondary ? 'linear-gradient(transparent 60%, var(--color-secondary) 60%)' :'linear-gradient(transparent 60%, var(--color-accent-100) 50%)'}`,
                 backgroundSize: '100% 1.2em',
                 backgroundRepeat: 'repeat',
                 backgroundPosition: '0 0'
