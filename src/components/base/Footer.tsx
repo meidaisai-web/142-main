d945bad2198f9489d85d5c95611dbe111df6d05c
yomi <itiger4649@gmail.com>
2026-09-15 20:43:11 +0900

    無駄なstyleを削除

--- src/components/base/Footer.tsx
+++ src/components/base/Footer.tsx
@@ -37,8 +37,7 @@ function FooterCloud({ isSubPage }: { isSubPage: boolean }) {
 
 	return (
 		<div
-			className="absolute z-10 left-0 w-full overflow-hidden"
-			style={{ aspectRatio: "1684 / 200", bottom: "calc(100% - 3px)" }}
+			className="absolute z-10 left-0 w-full overflow-hidden -translate-y-2/3"
 		>
 			<svg
 				viewBox="0 0 1684 414"
