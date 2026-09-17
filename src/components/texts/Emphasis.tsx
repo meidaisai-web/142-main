fb2699ee1fa131ff66e65f0746de25e9fa76af0b
SaraHoriguchi <shalaikukou@gmail.com>
2026-09-18 01:33:23 +0900

    emphasiscenterつけれるようにした

--- src/components/texts/Emphasis.tsx
+++ src/components/texts/Emphasis.tsx
@@ -4,14 +4,24 @@ type EmphasisProps = {
     children: React.ReactNode;
     bold?: boolean;
     secondary?: boolean;
+    center?: boolean;
+    className?: string;
 }
 
-export default function Emphasis({ children, bold = false, secondary = false }: EmphasisProps) {
-    return (
+export default function Emphasis({
+    children,
+    bold = false,
+    secondary = false,
+    center = false,
+    className = ""
+}: EmphasisProps) {
+    const emphasis = (
         <span
             className={`${bold ? 'text-xl font-bold' : 'font-semibold'}`}
             style={{
-                backgroundImage: `${secondary ? 'linear-gradient(transparent 60%, var(--color-secondary) 60%)' :'linear-gradient(transparent 60%, var(--color-accent-100) 50%)'}`,
+                backgroundImage: `${secondary
+                    ? 'linear-gradient(transparent 60%, var(--color-secondary) 60%)'
+                    : 'linear-gradient(transparent 60%, var(--color-accent-100) 50%)'}`,
                 backgroundSize: '100% 1.2em',
                 backgroundRepeat: 'repeat',
                 backgroundPosition: '0 0'
@@ -19,5 +29,19 @@ export default function Emphasis({ children, bold = false, secondary = false }: 
         >
             {children}
         </span>
-    )
+    );
+
+    if (center) {
+        return (
+            <div className={`text-center ${className}`}>
+                {emphasis}
+            </div>
+        );
+    }
+
+    return (
+        <span className={className}>
+            {children}
+        </span>
+    );
 }
